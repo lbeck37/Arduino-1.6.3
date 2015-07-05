b1e0515578ebc0ae56add9628c19345014838b49
Larry Beck <lbeck37@gmail.com>
2015-07-05 16:22:31 -0600

    Writing pump voltage to SD card

--- Sketches/Flojet/Flojet.ino
+++ Sketches/Flojet/Flojet.ino
@@ -1,7 +1,16 @@
 // 7/05/15A Flojet.ino sketch to use relays 1 and 2 in parallel to power FloJet on and off
 #include <Arduino.h>
 #include <Streaming.h>
-//#include <SD.h>
+#include <SPI.h>
+#include <SD.h>
+
+//SD card information
+// On the Ethernet Shield, CS is pin 4. Note that even if it's not
+// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
+// 53 on the Mega) must be left as an output or the SD library
+// functions will not work.
+static const int sChipSelect 						= 4;
+static const int sDefaultChipSelectPin	= 10;
 
 static const int 		asRelay[]  			= {0, 7, 6, 5, 4};  //Relay can be 1 to 4,no zero relay
 static const int  	sFirstRelay     = 1;
@@ -17,9 +26,11 @@ static const float	fMotorVoltsConvert  = 19.55;	//(4 * 5V *1000 / 1023)
 static long       lLineCount      = 0;  		//Serial Monitor uses for clarity.
 static int        sLastToggleSecsLeft;
 static long       lNextToggleMsec;      		//Next time to toggle pump relay.
+static long       lCurrentMsec;
 static boolean    bPumpIsOn;  							//Indicates current state of relays.
 static boolean    bPumpLoopRunning;					//loop() checks this
 static boolean		bPumpJustToggled;					//For logging.
+static float 			fMotorVoltage;
 
 /*
 Reads motor voltage at A0, scaled 4:1, 20V FS => 5V at A0.
@@ -36,6 +47,7 @@ void setup()  {
   Serial.begin(9600);
   sWaitForSerialMonitor();
   Serial << lLineCount++ << " setup(): Begin" << endl;
+  sSetupSD();
   sSetupPumpRelays();
   sClearPumpLoop();
   return;
@@ -49,6 +61,23 @@ int sWaitForSerialMonitor(){
   }
   return 1;
 }  //sWaitForSerialMonitor
+
+
+int sSetupSD(){
+	Serial << lLineCount++ <<" sSetupSD(): Initializing SD card"<< endl;
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(sDefaultChipSelectPin, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (SD.begin(sChipSelect)) {
+		Serial << lLineCount++ <<" sSetupSD(): SD card is setup"<< endl;
+  }
+  else {
+		Serial << lLineCount++ <<" sSetupSD(): SD card init failed, or not present"<< endl;
+	}
+  return 1;
+}  //sSetupSD
 
 
 void loop()  {
@@ -112,7 +141,7 @@ int sCheckKeyboard(){
 boolean bTimeToTogglePump(){
 	static int  sLastToggleSecsLeft	= 0;
   boolean 		bTogglePump;
-  long lCurrentMsec= millis();
+  lCurrentMsec= millis();
   if (lCurrentMsec > lNextToggleMsec) {
 		Serial << lLineCount++ << " bTimeToTogglePump(): lCurrentMsec/1000= "<< lCurrentMsec/1000
 					 <<", lNextToggleMsec/1000= "<< lNextToggleMsec/1000 << endl;
@@ -129,7 +158,7 @@ boolean bTimeToTogglePump(){
 int sPrintStatus(){
 	//Print once a second.
 	static long lLastPrintedMsec= 0;
-  long lCurrentMsec= millis();
+  lCurrentMsec= millis();
 	if ( ((lCurrentMsec - lLastPrintedMsec)/1000) > 0) {
 		lLastPrintedMsec= lCurrentMsec;
 		if (bPumpIsOn) {
@@ -144,24 +173,58 @@ int sPrintStatus(){
 		else {
 			Serial << "Seconds since start= " << (lCurrentMsec/1000) << endl;
 		}	//if(bPumpLoopRunning)else
-	sPrintMotorVolts();
+		sReadMotorVolts();
+		if (bPumpLoopRunning) {
+			//Want to be able to eject SD card when it's closed.
+			sLogMotorVolts();
+		}	//if(bPumpLoopRunning)
 	} //if(((lCurrentMsec...
   return 1;
 }  //sPrintStatus
 
 
-int sPrintMotorVolts(){
-	//static int sCount= 0;
-	//int sMotorVoltsRaw= 500 + ((sCount++ % 4) * 50);	//Test series
-	int   sMotorVoltsRaw= analogRead(sMotorVoltsPin);
-	float fMotorVoltage = (fMotorVoltsConvert * sMotorVoltsRaw) / 1000.0;
-	Serial << lLineCount++ << " sPrintMotorVolts(): Motor Volts= " << fMotorVoltage << endl;
+int sReadMotorVolts(){
+	int sMotorVoltsRaw= analogRead(sMotorVoltsPin);
+	fMotorVoltage = (fMotorVoltsConvert * sMotorVoltsRaw) / 1000.0;
+	Serial << lLineCount++ << " sReadMotorVolts(): Motor Volts= " << fMotorVoltage << endl;
   return 1;
-}  //sPrintMotorVolts
+}  //sReadMotorVolts
+
+
+int sLogMotorVolts(){
+#if 0
+	String	szLogLine= "Line count= ";
+	szLogLine += String(lLineCount);
+	szLogLine += ", Pump Volts= ";
+	szLogLine += String(fMotorVoltage);
+	Serial << lLineCount++ << " sLogMotorVolts(): szLogLine= " << szLogLine << endl;
+#endif
+	String	szLogLine= "";
+	szLogLine += String(lCurrentMsec);
+	szLogLine += ", ";
+	szLogLine += String(lLineCount);
+	if (bPumpIsOn) {
+		szLogLine += ", ON , ";
+	}
+	else {
+		szLogLine += ", OFF, ";
+	}
+	szLogLine += String(fMotorVoltage);
+
+  File LogFile= SD.open("PUMPLOG.txt", FILE_WRITE);
+  if (LogFile) {
+    LogFile.println(szLogLine);
+    LogFile.close();
+  }
+  else {
+		Serial << lLineCount++ << " sLogMotorVolts(): error opening " << "PUMPLOG.txt" << endl;
+  }
+  return 1;
+}  //sLogMotorVolts
 
 
 int sTogglePump(){
-  long lCurrentMsec= millis();
+  lCurrentMsec= millis();
   if (bPumpIsOn) {
     sTurnPumpOn(false);
     lNextToggleMsec= lCurrentMsec + lPumpOffMillis;
