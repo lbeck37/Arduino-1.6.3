0ac015d026775ba1e65cd371efc4483119cd3050
Larry Beck <lbeck37@gmail.com>
2015-07-24 11:33:45 -0600

    TCC running in loop but GPS is not connected nor is SD card code added.

--- Sketches/TinyCatCollar/TinyCatCollar.ino
+++ Sketches/TinyCatCollar/TinyCatCollar.ino
@@ -1,44 +1,22 @@
-// Sketch to use relays 1 and 2 in parallel to power FloJet on and off
-// 7/17B Remove SD code and simplify on Ace
-// 7/16 Move development from Sammy XP to Ace W7 because Sammy was crashing Arduno IDE.
-// 7/14/15 Remove check on motor voltage and add check on pressure switch.Ifdef SD card out.
+// TinyCatCollar.ino
+// 7/24/15 Port from Flojet.ino
 #include <Arduino.h>
 #include <Streaming.h>
 
 #define LOG0 lLineCount++ << " " << millis()
 
-static const int    asRelay[]       = {0, 7, 6, 5, 4};  //Relay can be 1 to 4,no zero relay
-static const int    sPressurePin    = 3;
-static const int    sFirstRelay     = 1;
-static const int    sLastRelay      = 2;
-static const int    sPumpOnSecs     = 360;
-static const int    sPumpOffSecs    = 600;
+static const int    sGPSPeriodSecs  = 1;
 static const long   lMsec           = 1000;
-static const long   lPumpOnMillis   = sPumpOnSecs  * lMsec;
-static const long   lPumpOffMillis  = sPumpOffSecs * lMsec;
+static const long   lGPSPeriodMsec  = sGPSPeriodSecs  * lMsec;
 
-static long       lLineCount      = 0;      //Serial Monitor uses for clarity.
-static int        sLastToggleSecsLeft;
-static long       lNextReadGPSMsec;          //Next time to toggle pump relay.
+static long       lLineCount      	= 0;      //Serial Monitor uses for clarity.
+static long       lNextReadGPSMsec;          	//Next time to read GPS.
 static long       lCurrentMsec;
-
-boolean           bPumpIsOn;                //Indicates current state of relays.
-boolean           bGPSLoopRunning;         //loop() checks this
-boolean           bPumpJustToggled;         //For logging.
-
-/*
-When board powers up pump is tuned off and pump loop is not running.
-Sketch uses Serial Monitor on a PC for output and input.
-Three keys may be pushed: R(un), S(top), T(oggle)
-Pressing R starts the pump, sets a time for toggling it off and starts the pump loop running.
-Pressing S stops the pump and turns off the pump loop.
-Pressing T toggles the pump on or off. If the pump loop is running it sets the toggle time
-and keeps the loop running. It does not start the loop if it's not on.
-*/
+static boolean    bGPSLoopRunning;         		//loop() checks this
 
 void setup()  {
   Serial.begin(9600);
-  sWaitForSerialMonitor();
+  //sWaitForSerialMonitor();
   Serial << LOG0 << " setup(): Begin" << endl;
   //sSetupSD();			//To be implemented.
  // sSetupGPS();		//To be implemented.
@@ -48,25 +26,21 @@ void setup()  {
 } //setup
 
 
-int sWaitForSerialMonitor(){
-  //The following (for Leonardo only) waits for SerialMonitor to start on PC.
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-  return 1;
-}  //sWaitForSerialMonitor
-
-
 void loop()  {
 	if (bGPSLoopRunning && bTimeToReadGPS()) {
     sReadGPS();
   } //if(bGPSLoopRunning&&...
-
   sPrintStatus();
   sCheckKeyboard();
   //sCheckWiFi();			//To be implemented.
   return;
 } //loop
+
+
+int sReadGPS(){
+  Serial << LOG0 << " sReadGPS(): Begin" << endl;
+  return 1;
+}  //sReadGPS
 
 
 int sClearGPSLoop(){
@@ -79,6 +53,7 @@ int sClearGPSLoop(){
 int sStartGPSLoop(){
 	sClearGPSLoop();
 	bGPSLoopRunning= true;
+  sSetNextGPSReadTime();
 	return 1;
 }  //sStartGPSLoop
 
@@ -87,6 +62,26 @@ int sStopGPSLoop(){
 	bGPSLoopRunning= false;
 	return 1;
 }  //sStopGPSLoop
+
+
+int sSetNextGPSReadTime(){
+  lNextReadGPSMsec= millis() + lGPSPeriodMsec;
+	return 1;
+}  //sSetNextGPSReadTime
+
+
+boolean bTimeToReadGPS(){
+  static int  sLastToggleSecsLeft = 0;
+  boolean     bReadGPS;
+  if (lCurrentMsec > lNextReadGPSMsec) {
+  	sSetNextGPSReadTime();
+    bReadGPS= true;
+  } //if(lCurrentMsec>lNextReadGPSMsec)
+  else {
+    bReadGPS= false;
+  } //else
+  return bReadGPS;
+}  //bTimeToReadGPS
 
 
 int sCheckKeyboard(){
@@ -120,49 +115,30 @@ int sCheckKeyboard(){
 }  //sCheckKeyboard
 
 
-boolean bTimeToReadGPS(){
-  static int  sLastToggleSecsLeft = 0;
-  boolean     bReadGPS;
-  if (lCurrentMsec > lNextReadGPSMsec) {
-    Serial << LOG0 << " bTimeToReadGPS(): lNextReadGPSMsec= "
-           << lNextReadGPSMsec << endl;
-    Serial << LOG0 <<" bTimeToReadGPS(): Setting bReadGPS to TRUE"<< endl;
-    bReadGPS= true;
-  } //if(lCurrentMsec>lNextReadGPSMsec)
-  else {
-    bReadGPS= false;
-  } //else
-  return bReadGPS;
-}  //bTimeToReadGPS
-
-
 int sPrintStatus(){
   //Print once a second.
   static long lLastPrintedMsec= 0;
   lCurrentMsec= millis();
   if ( ((lCurrentMsec - lLastPrintedMsec)/1000) > 0) {
     lLastPrintedMsec= lCurrentMsec;
-    if (bPumpIsOn) {
-      Serial << LOG0 << " sPrintStatus(): Pump is ON, ";
-    }
-    else {
-      Serial << LOG0 << " sPrintStatus(): Pump is OFF, ";
-    }
     if (bGPSLoopRunning) {
-      int lSecToToggle= (lNextReadGPSMsec-lCurrentMsec)/1000;
-      Serial <<"Seconds until pump toggle= "<< (lSecToToggle / 60) <<":"<< (lSecToToggle % 60) << endl;
-    } //if(bGPSLoopRunning)
+   		Serial << LOG0 <<" sPrintStatus(): GPS Loop is Running"<< endl;
+   } //if(bGPSLoopRunning)
     else {
       int lSecSinceStart= lCurrentMsec/1000;
-      Serial <<"Seconds since start= "<< (lSecSinceStart / 60) <<":"<< (lSecSinceStart % 60) << endl;
+      Serial << LOG0 <<" sPrintStatus(): Seconds since start= "<< (lSecSinceStart / 60) <<":"
+             << (lSecSinceStart % 60) << endl;
     } //if(bGPSLoopRunning)else
   } //if(((lCurrentMsec...
   return 1;
 }  //sPrintStatus
 
 
-int sReadGPS(){
-  Serial << LOG0 << " sReadGPS(): Begin" << endl;
+int sWaitForSerialMonitor(){
+  //The following (for Leonardo only) waits for SerialMonitor to start on PC.
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
   return 1;
-}  //sReadGPS
+}  //sWaitForSerialMonitor
 // Last line.
