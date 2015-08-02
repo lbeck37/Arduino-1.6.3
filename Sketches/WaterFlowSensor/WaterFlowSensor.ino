8c62bb07c690cf235ba29bf5102d5aaee9334669
lbeck37 <lbeck37@gmail.com>
2015-08-02 12:01:20 -0600

    Sammy got WaterFlowSensor.ino going on Leonardo

--- Sketches/WaterFlowSensor/WaterFlowSensor.ino
+++ Sketches/WaterFlowSensor/WaterFlowSensor.ino
@@ -1,15 +1,26 @@
-// 08/02/15 Beck
-/* YF-S201 Hall Effect Water Flow Meter / Sensor
-   http://www.hobbytronics.co.uk/yf-s201-water-flow-meter
-   Uses interrupt routine.
-   Read Water Flow Meter and output reading in litres/hour
-*/
+// 08/02/15 Modified to only read meter for one second.
+// YF-S201 Hall Effect Water Flow Meter / Sensor
+// http://www.hobbytronics.co.uk/yf-s201-water-flow-meter
 
-volatile int  sFlowCount;  // Measures flow meter pulses
-unsigned int  usLitersPerHour;          // Calculated litres/hour
-unsigned char ucFlowMeterPin = 2;  // Flow Meter Pin number
-unsigned long ulCurrentMsec;
-unsigned long ulLastPrintMsec;
+//Uses interrupt, Arduino pin must support interrupts.
+//see http://arduino.cc/en/Reference/attachInterrupt
+
+//Flowmeter produces 450 pulses/liter => 1,700 pulses/gallon
+//Expect 5 gal/minute which produces 8,500 pulses/min => 142 pulses/sec
+//Want to count for one second and calculate gal/min
+//Multiply 1 second count by 60 and divide by 1,700.
+
+#include <Arduino.h>
+#include <Streaming.h>
+
+#define LOG0            lLineCount++ << " " << millis()
+
+//const int     sPulsesPerGal= 1700;
+const float   fPulsesPerGal   = 1700.0;
+
+volatile int  sFlowCount;
+unsigned char ucFlowMeterPin  = 2;      // Flow Meter Pin number, must support interrupt.
+long          lLineCount      = 0;      //Serial Monitor uses for clarity.
 
 void vIncrementFlowCount()                  // Interruot function
 {
@@ -20,16 +31,13 @@ void vIncrementFlowCount()                  // Interruot function
 
 void setup()
 {
-   pinMode(ucFlowMeterPin, INPUT);
-   Serial.begin(9600);
-   //Turn on interrupt.
-   attachInterrupt(0, vIncrementFlowCount, RISING); // Setup Interrupt
-                                     // see http://arduino.cc/en/Reference/attachInterrupt
-   sei();                            // Enable interrupts
-   ulCurrentMsec= millis();
-   ulLastPrintMsec= ulCurrentMsec;
+  pinMode(ucFlowMeterPin, INPUT);
+  digitalWrite(ucFlowMeterPin, HIGH);              //Turn on internal pullup.
+  Serial.begin(9600);
+  Serial << LOG0 << " setup(): Attach interrupt to pin "<< ucFlowMeterPin << endl;
 
-   return;
+  attachInterrupt(0, vIncrementFlowCount, RISING); // Setup Interrupt
+  return;
 } //setup
 
 
@@ -37,17 +45,14 @@ void loop ()
 {
   //Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min. (Results in +/- 3% range)
   //(Pulse frequency x 60 min) / 7.5Q = flow rate in L/hour
-  //Datasheet shows:
-  //  Flow range: 100 L/Hr to 1800 L/Hr?
+  //Reset pulse count and turn on interrupts.
+  sFlowCount= 0;
+  sei();                            // Enable interrupts
+  delay(1000);                      //Count for one second.
+  cli();                            //Turn off interrupts.
 
-  ulCurrentMsec= millis();
-  // Every second, calculate and print litres/hour
-  if(ulCurrentMsec >= (ulLastPrintMsec + 1000)) {
-    ulLastPrintMsec= ulCurrentMsec;
-    usLitersPerHour= (sFlowCount * 60 / 7.5);
-    sFlowCount= 0;                   // Reset Counter
-    Serial.print(usLitersPerHour, DEC);            // Print litres/hour
-    Serial.println(" Liter/hour");
-  } //if(ulCurrentMsec>=...
+  float fGalPerMin= (sFlowCount * 60.0 / fPulsesPerGal);
+  Serial << LOG0 << " loop(): fGalPerMin= "<< fGalPerMin << endl;
+  return;
 } //loop
 //Last line.
