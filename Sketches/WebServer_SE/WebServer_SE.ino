eeb43da503f5caa1cde92f986e044081485ae80e
lbeck37 <lbeck37@gmail.com>
2015-08-04 17:27:34 -0600

    LBT cleaned up turning LED on and off in WebServer_SE.ino

--- Sketches/WebServer_SE/WebServer_SE.ino
+++ Sketches/WebServer_SE/WebServer_SE.ino
@@ -16,16 +16,26 @@ static long       lLineCount	= 0;      //Serial Monitor uses for clarity.
 const char		*szSSID 						= "P291spot";
 const char		*szPassword 				= "Qazqaz11";
 const int 		sLedPin							= 2;
+const int 		sD0LedPin						= 0;
 
 //Create an instance of the server and specify the port to listen on as an argument
 WiFiServer server(80);
 
 String szHTTP_Request;          // stores the HTTP request
-boolean bLEDOn = false;   // state of LED, off by default
+boolean bLedOn = false;   // state of LED, off by default
+
+const static long   	lD0LedStageMsec[] = { 1000,  250,   250,  250,   250,  250}; //Msec that LED is on or off
+const static boolean  bD0LedStageIsOn[] = {false, true, false, true, false, true};
+const static int			sD0LedLastStage 	= 6;
+static boolean				bD0LedIsOn				= false;
+static long       		lD0LedNextMsec;          //Next time to change stage of LED
+static int        		sD0CurrentStage;            //0 to sD0LedLastStage
+static long       		lCurrentMsec;
 
 void setup()
 {
 	sSetupWiFi();
+	//sSetupD0Led();
   pinMode(sLedPin, OUTPUT);       // LED on pin sLedPin
 	return;
 }	//setup
@@ -33,11 +43,11 @@ void setup()
 
 int sSetupWiFi() {
   Serial.begin(115200);
-  Serial << LOG0 << " setup(): Begin" << endl;
+  Serial << LOG0 << " sSetupWiFi(): Begin" << endl;
   delay(10);
 
   //Connect to WiFi network
-  Serial << LOG0 << " sesSetupWiFitup(): Connecting to "<< szSSID << endl;
+  Serial << LOG0 << " sSetupWiFitup(): Connecting to "<< szSSID << endl;
   WiFi.begin(szSSID, szPassword);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -48,15 +58,57 @@ int sSetupWiFi() {
 
   //Start the server
   server.begin();
-  Serial << LOG0 <<" setup(): Server started, my IP is  "<< WiFi.localIP() << endl;
+  Serial << LOG0 <<" sSetupWiFi(): Server started, my IP is  "<< WiFi.localIP() << endl;
 	return 1;
 }	//sSetupWiFi
+
+
+int sSetupD0Led() {
+  Serial << LOG0 << " sSetupD0Led(): Begin" << endl;
+	lCurrentMsec= millis();
+	pinMode(sD0LedPin, OUTPUT);       // LED on pin sLedPin
+	sD0CurrentStage= 0;
+	lD0LedNextMsec= lCurrentMsec + lD0LedStageMsec[sD0CurrentStage];
+  Serial << LOG0 << " sSetupD0Led(): Set lD0LedNextMsec to " << lD0LedNextMsec << endl;
+  Serial << LOG0 << " sSetupD0Led(): Set call sTurnOnD0Led() with "
+         << bD0LedStageIsOn[sD0CurrentStage] << endl;
+	sTurnOnD0Led(bD0LedStageIsOn[sD0CurrentStage]);
+	return 1;
+}	//sSetupD0Led
+
+
+int sTurnOnD0Led(boolean bOn) {
+	if (bOn) {
+	  digitalWrite(sLedPin, HIGH);
+	}
+	else {
+	  digitalWrite(sLedPin, LOW);
+	}
+	return 1;
+}	//sTurnOnD0Led
+
+
+int sCycleD0Led() {
+	lCurrentMsec= millis();
+	if (lCurrentMsec > lD0LedNextMsec) {
+		if (sD0CurrentStage < sD0LedLastStage) {
+			sD0CurrentStage++;
+		}
+		else {
+      Serial << LOG0 <<" sCycleD0Led(): Set D0 LED to stage Zero"<< endl;
+			sD0CurrentStage= 0;
+		}
+		lD0LedNextMsec= lCurrentMsec + lD0LedStageMsec[sD0CurrentStage];
+		sTurnOnD0Led(bD0LedStageIsOn[sD0CurrentStage]);
+	}
+	return 1;
+}	//sCycleD0Led
 
 
 void loop()
 {
     WiFiClient client = server.available();  // try to get client
-
+		//sCycleD0Led();
     if (client) {  // got client?
         boolean currentLineIsBlank = true;
         Serial << LOG0 <<" loop(): Got a Client"<< endl;
@@ -114,71 +166,30 @@ void ProcessCheckbox(WiFiClient cl)
 {
     if (szHTTP_Request.indexOf("LED2=2") > -1) {  // see if checkbox was clicked
         // the checkbox was clicked, toggle the LED
-        if (bLEDOn) {
-            bLEDOn = 0;
+  			Serial << LOG0 <<" ProcessCheckbox(): Checkbox was clicked"<< endl;
+        if (bLedOn) {
+   				Serial << LOG0 <<" ProcessCheckbox(): Set bLedOn to FALSE"<< endl;
+          bLedOn = false;
         }
         else {
-            bLEDOn = 1;
+   				Serial << LOG0 <<" ProcessCheckbox(): Set bLedOn to TRUE"<< endl;
+          bLedOn = true;
         }
     }
 
-    if (bLEDOn) {    // switch LED on
+    if (bLedOn) {    // switch LED on
+  			Serial << LOG0 <<" ProcessCheckbox(): Turn external LED ON"<< endl;
         digitalWrite(sLedPin, HIGH);
         // checkbox is checked
         cl.println("<input type=\"checkbox\" name=\"LED2\" value=\"2\" \
         onclick=\"submit();\" checked>LED2");
     }
     else {              // switch LED off
+  			Serial << LOG0 <<" ProcessCheckbox(): Turn external LED OFF"<< endl;
         digitalWrite(sLedPin, LOW);
         // checkbox is unchecked
         cl.println("<input type=\"checkbox\" name=\"LED2\" value=\"2\" \
         onclick=\"submit();\">LED2");
     }
 }	//ProcessCheckbox
-
-/* void loop()
-{
-    WiFiClient client = server.available();  // try to get client
-    if (client) {  // got client?
-        boolean currentLineIsBlank = true;
-        while (client.connected()) {
-            if (client.available()) {   // client data available to read
-                char c = client.read(); // read 1 byte (character) from client
-                // last line of client request is blank and ends with \n
-                // respond to client only after last line received
-                if (c == '\n' && currentLineIsBlank) {
-                    // send a standard http response header
-                    client.println("HTTP/1.1 200 OK");
-                    client.println("Content-Type: text/html");
-                    client.println("Connection: close");
-                    client.println();
-                    // send web page
-                    client.println("<!DOCTYPE html>");
-                    client.println("<html>");
-                    client.println("<head>");
-                    client.println("<title>Arduino Web Page</title>");
-                    client.println("</head>");
-                    client.println("<body>");
-                    client.println("<h1>Hello from FloJet!</h1>");
-                    client.println("<p>A web page from Larry Beck's FloJet Arduino server</p>");
-                    client.println("</body>");
-                    client.println("</html>");
-                    break;
-                }	//if(c=='\n'&&...
-                //Every line of text received from the client ends with \r\n
-                if (c == '\n') {
-                    // last character on line of received text
-                    // starting new line with next character read
-                    currentLineIsBlank = true;
-                }	//if(c=='\n')
-                else if (c != '\r') {
-                    // a text character was received from client
-                    currentLineIsBlank = false;
-                }	//elseif(c!='\r')
-            } //if(client.available())
-        } //while (client.connected())
-        delay(1);      // give the web browser time to receive the data
-        client.stop(); // close the connection
-    } //if(client.available())
-}	//loop*/
 //Last line
