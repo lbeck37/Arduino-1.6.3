862655a0fecb2d8e02adea981a9b30d4872a8e36
lbeck37 <lbeck37@gmail.com>
2015-08-04 12:45:52 -0600

    LBT got WebServer_SE.ino going

--- Sketches/WebServer_SE/WebServer_SE.ino
+++ Sketches/WebServer_SE/WebServer_SE.ino
@@ -1,44 +1,57 @@
-*--------------------------------------------------------------
+// 08/04/15 Ported to ESP8266
+/*--------------------------------------------------------------
   Program:      eth_websrv_page
-
-  Description:  Arduino web server that serves up a basic web
-                page. Does not use the SD card.
-
-  Hardware:     Arduino Uno and official Arduino Ethernet
-                shield. Should work with other Arduinos and
-                compatible Ethernet shields.
-
-  Software:     Developed using Arduino 1.0.3 software
-                Should be compatible with Arduino 1.0 +
-
   References:   - WebServer example by David A. Mellis and
                   modified by Tom Igoe
-                - Ethernet library documentation:
-                  http://arduino.cc/en/Reference/Ethernet
-
   Date:         7 January 2013
-
   Author:       W.A. Smith, http://startingelectronics.org
 --------------------------------------------------------------*/
+#include <Arduino.h>
+#include <Streaming.h>
+#include <ESP8266WiFi.h>
 
-#include <SPI.h>
-#include <Ethernet.h>
+#define LOG0      lLineCount++ << " " << millis()
+static long       lLineCount	= 0;      //Serial Monitor uses for clarity.
 
-// MAC address from Ethernet shield sticker under board
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
-EthernetServer server(80);  // create a server at port 80
+const char* szSSID 						= "P291spot";
+const char* szPassword 				= "Qazqaz11";
+
+//Create an instance of the server and specify the port to listen on as an argument
+WiFiServer server(80);
 
 void setup()
 {
-    Ethernet.begin(mac, ip);  // initialize Ethernet device
-    server.begin();           // start to listen for clients
-}
+	sSetupWiFi();
+	return;
+}	//setup
+
+
+int sSetupWiFi() {
+  Serial.begin(115200);
+  Serial << LOG0 << " setup(): Begin" << endl;
+  delay(10);
+
+	WiFi.begin(szSSID, szPassword);
+  //Connect to WiFi network
+  Serial << LOG0 << " setup(): Connecting to "<< szSSID << endl;
+  WiFi.begin(szSSID, szPassword);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial << endl << LOG0 <<" setup(): WiFi connected"<< endl;
+
+  //Start the server
+  server.begin();
+  Serial << LOG0 <<" setup(): Server started, my IP is  "<< WiFi.localIP() << endl;
+	return 1;
+}	//sSetupWiFi
+
 
 void loop()
 {
-    EthernetClient client = server.available();  // try to get client
-
+    WiFiClient client = server.available();  // try to get client
     if (client) {  // got client?
         boolean currentLineIsBlank = true;
         while (client.connected()) {
@@ -59,24 +72,26 @@ void loop()
                     client.println("<title>Arduino Web Page</title>");
                     client.println("</head>");
                     client.println("<body>");
-                    client.println("<h1>Hello from Arduino!</h1>");
-                    client.println("<p>A web page from the Arduino server</p>");
+                    client.println("<h1>Hello from FloJet!</h1>");
+                    client.println("<p>A web page from Larry Beck's FloJet Arduino server</p>");
                     client.println("</body>");
                     client.println("</html>");
                     break;
-                }
-                // every line of text received from the client ends with \r\n
+                }	//if(c=='\n'&&...
+                //Every line of text received from the client ends with \r\n
                 if (c == '\n') {
                     // last character on line of received text
                     // starting new line with next character read
                     currentLineIsBlank = true;
-                }
+                }	//if(c=='\n')
                 else if (c != '\r') {
                     // a text character was received from client
                     currentLineIsBlank = false;
-                }
-            } // end if (client.available())
-        } // end while (client.connected())
+                }	//elseif(c!='\r')
+            } //if(client.available())
+        } //while (client.connected())
         delay(1);      // give the web browser time to receive the data
         client.stop(); // close the connection
-    } // end if (client)
+    } //if(client.available())
+}	//loop
+//Last line
