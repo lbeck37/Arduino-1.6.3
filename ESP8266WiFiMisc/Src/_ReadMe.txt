8fdd9de0de4c604b8cfadcf8de594500a6423312
lbeck37 <lbeck37@gmail.com>
2015-08-08 14:43:15 -0600

    V64 added ESP8266 .h and .cpp files in ESP8266iFiMisc directory

---
+++ ESP8266WiFiMisc/Src/_ReadMe.txt
@@ -0,0 +1,3 @@
+8/8/15 Pulled from Vista64 desktop computer:
+   C:\Users\PC\AppData\Roaming\Arduino15\packages\esp8266\hardware\esp8266\
+      1.6.5-947-g39819f0\libraries\ESP8266WiFi\src
--- Sketches/ESPWebServerAP_SF/ESPWebServerAP_SF.ino
+++ Sketches/ESPWebServerAP_SF/ESPWebServerAP_SF.ino
@@ -15,7 +15,7 @@ const char WiFiAPPSK[] = "sparkfun";
 /////////////////////
 // Pin Definitions //
 /////////////////////
-const int LED_PIN       = 5;        // Thing's onboard, green LED pulled low though 200 ohm
+const int LED_PIN       = 5;        // Thing'szResponse onboard, green LED pulled low though 200 ohm
 const int ANALOG_PIN    = A0;       // The only analog pin on the Thing
 const int DIGITAL_PIN   = 12;       // Digital pin to be read
 
@@ -24,6 +24,8 @@ WiFiServer server(80);
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial << LOG0 <<" setup(): Begin"<< endl;
   initHardware();
   setupWiFi();
   server.begin();
@@ -34,61 +36,64 @@ void setup()
 void loop()
 {
   // Check if a client has connected
-  WiFiClient client = server.available();
+  WiFiClient client= server.available();
   if (!client) {
     return;
-  }
+  } //if(!client)
 
+  Serial << LOG0 <<" loop(): server.available() returned a Client" << endl;
   // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
+  String szRequest = client.readStringUntil('\r');
+  //Serial.println(szRequest);
+  Serial << LOG0 <<" loop(): Request = |"<< szRequest <<"|"<< endl;
   client.flush();
 
   // Match the request
-  int val = -1; // We'll use 'val' to keep track of both the
+  int sOperation= -1; // We'll use 'sOperation' to keep track of both the
                 // request type (read/set) and value if set.
-  if (req.indexOf("/led/0") != -1)
-    val = 0; // Will write LED low
-  else if (req.indexOf("/led/1") != -1)
-    val = 1; // Will write LED high
-  else if (req.indexOf("/read") != -1)
-    val = -2; // Will print pin reads
+  if (szRequest.indexOf("/led/0") != -1)
+    sOperation= 0; // Will write LED low
+  else if (szRequest.indexOf("/led/1") != -1)
+    sOperation= 1; // Will write LED high
+  else if (szRequest.indexOf("/read") != -1)
+    sOperation= -2; // Will print pin reads, it'szResponse negative so digitalWrite(LED_PIN, -2) does nothing
   // Otherwise request will be invalid. We'll say as much in HTML
 
   // Set LED according to the request
-  if (val >= 0)
-    digitalWrite(LED_PIN, val);
+  if (sOperation >= 0)
+    digitalWrite(LED_PIN, sOperation);
 
   client.flush();
 
   // Prepare the response. Start with the common header:
-  String s = "HTTP/1.1 200 OK\r\n";
-  s += "Content-Type: text/html\r\n\r\n";
-  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  String szResponse = "HTTP/1.1 200 OK\r\n";
+  szResponse += "Content-Type: text/html\r\n\r\n";
+  szResponse += "<!DOCTYPE HTML>\r\n<html>\r\n";
   // If we're setting the LED, print out a message saying we did
-  if (val >= 0)
+  if (sOperation >= 0)
   {
-    s += "LED is now ";
-    s += (val)?"on":"off";
+    szResponse += "LED is now ";
+    szResponse += (sOperation)?"on":"off";
   }
-  else if (val == -2)
+  else if (sOperation == -2)
   { // If we're reading pins, print out those values:
-    s += "Analog Pin = ";
-    s += String(analogRead(ANALOG_PIN));
-    s += "<br>"; // Go to the next line.
-    s += "Digital Pin 12 = ";
-    s += String(digitalRead(DIGITAL_PIN));
+    szResponse += "Analog Pin = ";
+    szResponse += String(analogRead(ANALOG_PIN));
+    szResponse += "<br>"; // Go to the next line.
+    szResponse += "Digital Pin 12 = ";
+    szResponse += String(digitalRead(DIGITAL_PIN));
   }
   else
   {
-    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
+    szResponse += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
   }
-  s += "</html>\n";
+  szResponse += "</html>\n";
 
   // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
+  Serial << LOG0 <<" loop(): Send Response= |"<< szResponse <<"|"<< endl;
+  client.print(szResponse);
+  //delay(1);
+  //Serial.println("Client disonnected");
 
   // The client will actually be disconnected
   // when the function returns and 'client' object is detroyed
@@ -113,23 +118,23 @@ void setupWiFi()
   char AP_NameChar[AP_NameString.length() + 1];
   memset(AP_NameChar, AP_NameString.length() + 1, 0);
 
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
+  for (int i=0; i < AP_NameString.length(); i++) {
+    AP_NameChar[i]= AP_NameString.charAt(i);
+  } //for
 
-  Serial << LOG0 << " setupWiFi(): Call WiFi.softAP(AP_NameChar, WiFiAPPSK)" << endl;
-  return;
+  Serial << LOG0 <<" setupWiFi(): Call WiFi.softAP("<< AP_NameChar <<", "<< WiFiAPPSK <<")" << endl;
   WiFi.softAP(AP_NameChar, WiFiAPPSK);
+  return;
 } //setupWiFi
 
 
 void initHardware()
 {
-  Serial.begin(115200);
   Serial << LOG0 << " initHardware(): Begin" << endl;
   pinMode(DIGITAL_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
   // Don't need to set ANALOG_PIN as input,
-  // that's all it can be.
+  // that'szResponse all it can be.
 } //initHardware
 //Last line.
