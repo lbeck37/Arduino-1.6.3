45b9fc3b8ba7853d9bb9acf6800259b26d3b81b3
lbeck37 <lbeck37@gmail.com>
2015-08-08 06:56:44 -0600

    V64 used Blink to verify that Adafruit ESP8266 breakout works as long as Vcc gets 3.3V from seperate regulator and not from FTDI USB adapter

--- Sketches/Blink_Adafruit_ESP8266_debug/Blink_Adafruit_ESP8266_debug.ino
+++ Sketches/Blink_Adafruit_ESP8266_debug/Blink_Adafruit_ESP8266_debug.ino
@@ -1,17 +1,12 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
-
 void setup() {
 	pinMode(0, OUTPUT);
 }	//setup
 
 void loop() {
 	digitalWrite(0, HIGH);
-  delay(1500);
-  // delay(500);
+  //delay(1500);
+  delay(1000);
 	digitalWrite(0, LOW);
-	delay(500);
+  //delay(500);
+  delay(3000);
 }	//loop
