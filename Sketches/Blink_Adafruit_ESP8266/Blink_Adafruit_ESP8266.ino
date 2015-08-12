7e16581de7cc92a5acdd56e71beae78c9ad53f87
lbeck37 <lbeck37@gmail.com>
2015-08-12 09:33:00 -0600

    LBT modifies Blink_Adafruit_ESP8266.ino

--- Sketches/Blink_Adafruit_ESP8266/Blink_Adafruit_ESP8266.ino
+++ Sketches/Blink_Adafruit_ESP8266/Blink_Adafruit_ESP8266.ino
@@ -9,16 +9,16 @@ void setup() {
   Serial.begin(115200);
   //sWaitForSerialMonitor();
   Serial << LOG0 << " setup(): Begin" << endl;
-  pinMode(0, OUTPUT);
+  //pinMode(0, OUTPUT);
 }  //setup
 
 void loop() {
    Serial << LOG0 << " loop(): Begin" << endl;
-   digitalWrite(0, HIGH);
-   delay(1500);
-   // delay(500);
-   digitalWrite(0, LOW);
+   //digitalWrite(0, HIGH);
+   //delay(1500);
    delay(500);
+   //digitalWrite(0, LOW);
+   //delay(500);
 }  //loop
 
 
