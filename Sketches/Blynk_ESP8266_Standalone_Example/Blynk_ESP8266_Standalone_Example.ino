db3522ad08e3dc8c2abc31fc06c2205a2a9410c2
lbeck37 <lbeck37@gmail.com>
2015-11-23 20:26:29 -0700

    Thermisor running but always reading 1023

--- Sketches/Blynk_ESP8266_Standalone_Example/Blynk_ESP8266_Standalone_Example.ino
+++ Sketches/Blynk_ESP8266_Standalone_Example/Blynk_ESP8266_Standalone_Example.ino
@@ -26,7 +26,7 @@
  *
  **************************************************************/
 
-#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+//#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
