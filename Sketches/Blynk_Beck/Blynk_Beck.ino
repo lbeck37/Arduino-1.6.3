1ee0327eb24b193297849999ae95708baa82debb
lbeck37 <lbeck37@gmail.com>
2015-12-13 19:21:58 -0700

    Fireplace working on NodeMCU

--- Sketches/Blynk_Beck/Blynk_Beck.ino
+++ Sketches/Blynk_Beck/Blynk_Beck.ino
@@ -1,5 +1,5 @@
 static const char szSketchName[]  = "Blynk_Beck.ino";
-static const char szFileDate[]    = "Dec 13, 2015B";
+static const char szFileDate[]    = "Dec 13, 2015G";
 // 12/13/15 Merge in support for Fireplace.
 // 12/12/15 Created from Blynk_LightTimer.ino
 
@@ -23,13 +23,15 @@ static const char szFileDate[]    = "Dec 13, 2015B";
 #define DegF_V3          V3
 #define RelayState_V5    V5
 #define RelayState_V6    V6
-#define LCD_Line0_V7     V7
-#define LCD_Line1_V8     V8
-#define Terminal_V9      V9
 #define Button_V10       V10   //Relay 0
 #define Timer_V11        V11   //Relay 0
-#define Button_V20       V20   //Relay 1
-#define Timer_V21        V21   //Relay 1
+#define Button_V12       V12   //Relay 1
+#define Timer_V13        V13   //Relay 1
+#define LCD_Line0_V20    V20
+#define LCD_Line1_V21    V21
+#define Terminal_V22     V22
+#define LED_V23          V23
+#define LED_V24          V24
 
 #define LOG0      lLineCount++ << " " << millis()
 
@@ -61,9 +63,10 @@ static const int    sOneWirePin           = 12;   //Dallas DS18B20 Temperature S
   static int sSketchType= sGarage;
 #endif
 
-//Attach virtual serial terminal to Virtual Pin V9
-WidgetTerminal      terminal(Terminal_V9);
+WidgetTerminal      oTerminal(Terminal_V22);
 WidgetLCD           LCDWidget(1);
+WidgetLED           oLED1(LED_V23);
+WidgetLED           oLED2(LED_V24);
 
 //Maxim/Dallas OneWire sensors
 /* Set up a oneWire instance to communicate with any OneWire device*/
@@ -83,7 +86,6 @@ void setup()
 {
   Serial.begin(9600);
   Serial << endl << LOG0 << " setup(): Initialized serial to 9600 baud" << endl;
-  //Serial << LOG0 << " setup(): Sketch: " << szSketchName << ", " << szFileDate << endl;
   Serial << LOG0 << " setup(): Sketch: " << szSketchName << "/" << szSketchType << ", " << szFileDate << endl;
   Wire.begin();
 
@@ -108,52 +110,38 @@ void loop()
 {
   Blynk.run();
 } //loop
-  // This will print Blynk Software version to the Terminal Widget when
-  // your hardware gets connected to Blynk Server
+
+
+void vHandleBlynkLEDs(){
+  if (sRelayState[0]){
+    Serial << LOG0 << " vHandleBlynkLEDs(): Call oLED1.off()" << endl;
+    oLED1.off();
+  } //if(sRelayState[0])
+  else{
+    Serial << LOG0 << " vHandleBlynkLEDs(): Call oLED1.on()" << endl;
+    oLED1.on();
+  } //if(sRelayState[0])else
+  if (sRelayState[1]){
+    Serial << LOG0 << " vHandleBlynkLEDs(): Call oLED2.off()" << endl;
+    oLED2.off();
+  } //if(sRelayState[1])
+  else{
+    Serial << LOG0 << " vHandleBlynkLEDs(): Call oLED2.on()" << endl;
+    oLED2.on();
+  } //if(sRelayState[1])else
+  return;
+} //vHandleBlynkLEDs
 
 
 int sTerminalPrintVersion(){
   Serial << LOG0 << " sTerminalPrintVersion: Begin" << endl;
-  terminal.println(F("Blynk v" BLYNK_VERSION ": Device started"));
-  terminal.println("-------------");
-  terminal.println("Type 'Marco' and get a reply, or type");
-  terminal.println("anything else and get it printed back.");
-  terminal.flush();
+  oTerminal.println(F("Blynk v" BLYNK_VERSION ": Device started"));
+  oTerminal.println("-------------");
+  oTerminal.println("Type 'Marco' and get a reply, or type");
+  oTerminal.println("anything else and get it printed back.");
+  oTerminal.flush();
   return 1;
 } //sTerminalPrintVersion
-
-
-// You can send commands from Terminal to your hardware. Just use
-// the same Virtual Pin as your Terminal Widget
-//int sWriteTerminalLine(char *szString){
-int sWriteTerminalLine(String szString){
-  Serial << LOG0 << " sWriteTerminalLine: Received String= " <<  szString << endl;
-
-  terminal.println(szString) ;
-
-  // Ensure everything is sent
-  terminal.flush();
-  return 1;
-} //sWriteTerminalLine
-
-
-int sSetupRelays(){
-  Serial << LOG0 << " sSetupRelays: Call pinMode() & sSetRelay() to enable OUTPUT on both pins" << endl;
-  for (int sRelay= 0; sRelay < sNumRelays; sRelay++){
-    pinMode(sRelayPin[sRelay], OUTPUT);
-    sSetRelay(sRelay, sRelayOpen);
-  } //for
-  return 1;
-} //sSetupRelays
-
-
-int sSetRelay(int sRelay, int sRelaySetting){
-  //Serial << LOG0 << " sSetRelay: sRelay= " << sRelay << endl;
-  Serial << LOG0 << " sSetRelay: Call digitalWrite to set relay on Pin " << sRelayPin[sRelay] << " to " << sRelaySetting << endl;
-  digitalWrite(sRelayPin[sRelay], sRelaySetting);
-  sRelayState[sRelay]= sRelaySetting;
-  return 1;
-} //sSetRelay
 
 
 int sBlynkLog(String szString, int sValue){
@@ -171,6 +159,52 @@ int sBlynkLog(String szString, int sValue){
 } //sBlynkLog
 
 
+int sBlynkLog(String szString, float fValue){
+  long lMillis= millis();
+  long lCurrentLine= lLineCount2++;
+
+  String szTermString= "";
+  szTermString += lCurrentLine;
+  szTermString += " ";
+  szTermString += lMillis;
+  szTermString += szString;
+  szTermString +=  fValue;
+  sWriteTerminalLine(szTermString);
+  return 1;
+} //sBlynkLog
+
+
+// You can send commands from Terminal to your hardware. Just use
+// the same Virtual Pin as your Terminal Widget
+//int sWriteTerminalLine(char *szString){
+int sWriteTerminalLine(String szString){
+  Serial << LOG0 << " sWriteTerminalLine: Call oTerminal.println(" << szString << ")" << endl;
+  oTerminal.println(szString) ;
+  // Ensure everything is sent
+  oTerminal.flush();
+  return 1;
+} //sWriteTerminalLine
+
+
+int sSetupRelays(){
+  Serial << LOG0 << " sSetupRelays: Call pinMode() & sSetRelay() to enable OUTPUT on both pins" << endl;
+  for (int sRelay= 0; sRelay < sNumRelays; sRelay++){
+    pinMode(sRelayPin[sRelay], OUTPUT);
+    sSetRelay(sRelay, sRelayOpen);
+  } //for
+  return 1;
+} //sSetupRelays
+
+
+int sSetRelay(int sRelay, int sRelaySetting){
+  Serial << LOG0 << " sSetRelay: Call digitalWrite to set relay on Pin " << sRelayPin[sRelay] << " to " << sRelaySetting << endl;
+  digitalWrite(sRelayPin[sRelay], sRelaySetting);
+  sRelayState[sRelay]= sRelaySetting;
+  vHandleBlynkLEDs();
+  return 1;
+} //sSetRelay
+
+
 float fGetDegC() {
   oSensors.requestTemperatures(); // Send the command to get temperatures
   float fDegC= oSensors.getTempCByIndex(0);
@@ -179,8 +213,9 @@ float fGetDegC() {
 
 
 float fGetDegF() {
+  float fDegF= 37.37;   //Value used for default in testing w/o reading sensor.
   oSensors.requestTemperatures(); // Send the command to get temperatures
-  float fDegF= oSensors.getTempFByIndex(0);
+  fDegF= oSensors.getTempFByIndex(0);
   return fDegF;
 }  //fGetDegF
 
@@ -192,12 +227,6 @@ float fRound(float fNum) {
 }  //fRound
 
 
-/*
-  switch sSketchType{
-    case :
-      break;
-  } //switch
-  */
 //BLYNK_READ() functions are called by the Blynk app on the phone (at a 1 second interval)
 //and returns the value or state of some variable.
 //BLYNK_WRITE() functions are called by the Blynk app on the phone
@@ -212,13 +241,16 @@ BLYNK_READ(DegC_V1){
 BLYNK_READ(DegF_V2){
   float fDegF= fGetDegF();
   Serial << LOG0 << " BLYNK_READ(DegF_V2): fGetDegF() returned " << fDegF << endl;
+
+  String szString= " DegF_V2: ";
+  sBlynkLog(szString, fDegF);
+
   Blynk.virtualWrite(DegF_V2, fRound(fDegF));
 } //BLYNK_READ(DegF_V2)
 
 
 BLYNK_READ(DegF_V3){
   float fDegF= fGetDegF();
-  //Serial << LOG0 << " BLYNK_READ(DegF_V3): fGetDegF() returned " << fDegF << endl;
   Blynk.virtualWrite(DegF_V3, fDegF);
 } //BLYNK_READ(DegF_V3)
 
@@ -231,28 +263,6 @@ BLYNK_READ(RelayState_V5){
 BLYNK_READ(RelayState_V6){
   Blynk.virtualWrite(RelayState_V6, !sRelayState[1]);
 } //BLYNK_READ(RelayState_V6)
-
-
-BLYNK_WRITE(Terminal_V9)
-{
-  Serial << LOG0 << " BLYNK_WRITE(Terminal_V9): Received Parameter= " <<  param.asStr() << endl;
-
-  // if you type "Marco" into Terminal Widget - it will respond: "Polo:"
-  if (String("Marco") == param.asStr()) {
-      terminal.println("You said: 'Marco'") ;
-      terminal.println("I said: 'Polo'") ;
-  } else {
-
-  // Send it back
-  terminal.print("You said:");
-  terminal.write(param.getBuffer(), param.getLength());
-  terminal.println();
-  }
-
-  // Ensure everything is sent
-  terminal.flush();
-  return;
-} //BLYNK_WRITE(Terminal_V9)
 
 
 //Handler callback function called when Button set as a Switch is pressed.
@@ -268,13 +278,13 @@ BLYNK_WRITE(Button_V10){
 
   String szString= " Button_V10: ";
   sBlynkLog(szString, sSetting);
-
+/*
   //Test writing to LCD
   LCDWidget.clear();
   int sCharPos= 0;   //Position 0-15
   int sLineNum= 0;   //Line 0-1
   LCDWidget.print(0, 0, "Relay #0 set to: ");
-
+*/
   if (sSetting == 1){
     sRelaySetting= sRelayClosed;
   }
@@ -306,13 +316,13 @@ BLYNK_WRITE(Timer_V11){
   } //BLYNK_WRITE(Timer_V11)
 
 
-BLYNK_WRITE(Button_V20){
+BLYNK_WRITE(Button_V12){
   int sSetting= param.asInt();
   int sRelaySetting;
-  Serial << LOG0 << " BLYNK_WRITE(Button_V20): Received Parameter= " << sSetting << endl;
+  Serial << LOG0 << " BLYNK_WRITE(Button_V12): Received Parameter= " << sSetting << endl;
   Serial << LOG0 << " ******* Blynk Button for Relay #1 ******" << endl;
 
-  String szString= " Button_V20: ";
+  String szString= " Button_V12: ";
   sBlynkLog(szString, sSetting);
 
   if (sSetting == 1){
@@ -323,16 +333,16 @@ BLYNK_WRITE(Button_V20){
   }
   sSetRelay(1, sRelaySetting);    //Set relay #1
   return;
-} //BLYNK_WRITE(Button_V20)
+} //BLYNK_WRITE(Button_V12)
 
 
-BLYNK_WRITE(Timer_V21){
+BLYNK_WRITE(Timer_V13){
   int sSetting= param.asInt();
   int sRelaySetting;
-  Serial << LOG0 << " BLYNK_WRITE(Timer_V21): Received Parameter= " << sSetting << endl;
+  Serial << LOG0 << " BLYNK_WRITE(Timer_V13): Received Parameter= " << sSetting << endl;
   Serial << LOG0 << " ******* Blynk Timer for Relay #1 ******" << endl;
 
-  String szString= " Timer_V21: ";
+  String szString= " Timer_V13: ";
   sBlynkLog(szString, sSetting);
 
   if (sSetting == 1){
@@ -343,5 +353,27 @@ BLYNK_WRITE(Timer_V21){
   }
   sSetRelay(1, sRelaySetting);    //Set relay #1
   return;
-} //BLYNK_WRITE(Timer_V21)
+} //BLYNK_WRITE(Timer_V13)
+
+/*
+BLYNK_WRITE(Terminal_V22)
+{
+  Serial << LOG0 << " BLYNK_WRITE(Terminal_V22): Received Parameter= " <<  param.asStr() << endl;
+  // if you type "Marco" into Terminal Widget - it will respond: "Polo:"
+  if (String("Marco") == param.asStr()) {
+      oTerminal.println("You said: 'Marco'") ;
+      oTerminal.println("I said: 'Polo'") ;
+  } else {
+
+  // Send it back
+  oTerminal.print("You said:");
+  oTerminal.write(param.getBuffer(), param.getLength());
+  oTerminal.println();
+  }
+
+  // Ensure everything is sent
+  oTerminal.flush();
+  return;
+} //BLYNK_WRITE(Terminal_V22)
+*/
 //Last line.
