0854cc59fa07315fa78030963ca47bc16f21b86d
lbeck37 <lbeck37@gmail.com>
2016-01-05 17:51:43 -0700

    V64 BeckWebUpdater working

--- Eclipse/Workspace/BeckWebUpdater/BeckWebUpdater.ino
+++ Eclipse/Workspace/BeckWebUpdater/BeckWebUpdater.ino
@@ -1,14 +1,19 @@
-/*
-  To upload through terminal you can use: curl -F "image=@firmware.bin" esp8266-webupdate.local/update
-*/
+static const char szSketchName[]  = "BeckWebUpdater.ino";
+static const char szFileDate[]    = "Jan 5, 2016A";
+// 1/5/16 Get running on V64 eclipseArduino
+// To upload through terminal you can use: curl -F "image=@firmware.bin" esp8266-webupdate.local/update
 
+//#include <Streaming.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
 
-const char* host = "esp42";
+//#define LOG0    szLogLineHeader(++lLineCount)
+//static long         lLineCount            = 0;      //Serial Monitor uses for clarity.
+
+const char* host = "esp37F";
 const char* ssid = "Aspot24";
 const char* password = "Qazqaz11";
 
@@ -19,7 +24,7 @@ void setup(void){
 
   Serial.begin(115200);
   Serial.println();
-  Serial.println("Booting BeckOTA2.bin 1/5/16C");
+  Serial.println("Booting BeckOTA2.bin 1/5/16F");
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, password);
 
@@ -41,3 +46,17 @@ void loop(void){
   httpServer.handleClient();
   delay(1);
 }
+
+
+/*String szLogLineHeader(long lLineCount){
+  String szHeader= "";
+  szHeader += lLineCount;
+  szHeader += " ";
+  //szTermString += szTime;
+  szHeader += szGetTime(millis());
+  //szHeader += " ";
+  return szHeader;
+} //szLogLineHeader
+*/
+
+
