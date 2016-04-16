beb0222c5e503bcd89824e17222db452c413975f
lbeck37 <lbeck37@gmail.com>
2016-04-15 21:07:37 -0600

    HP7 BeckWebUpdater minor while debugging

--- Sketches/BeckWebUpdater/BeckWebUpdater.ino
+++ Sketches/BeckWebUpdater/BeckWebUpdater.ino
@@ -1,5 +1,5 @@
 static const char acSketchName[]  = "BeckWebUpdater.ino";
-static const char acFileDate[]    = "Apr 14, 2016AA";
+static const char acFileDate[]    = "Apr 15, 2016A";
 // 1/5/16 Get running on V64 eclipseArduino
 
 #include <BeckLib.h>
