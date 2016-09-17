73069790ecbb9a59e70ade68d23a37537cd51884
lbeck37 <lbeck37@gmail.com>
2016-09-16 19:37:28 -0600

    HP7 on BeckESP8266Base address

--- Sketches/BeckESP8266Base/BeckESP8266Base.ino
+++ Sketches/BeckESP8266Base/BeckESP8266Base.ino
@@ -1,5 +1,5 @@
 String acSketchName  = "BeckESP8266Base.ino";
-String acFileDate    = "Sept 16, 2016_V64_A";		//Was May 15, 2016_HP7N, June 19, 2016_LBT_D
+String acFileDate    = "Sept 16, 2016_HP7_A";		//Was May 15, 2016_HP7N, June 19, 2016_LBT_D
 
 //#define TEST_WIFI
 
