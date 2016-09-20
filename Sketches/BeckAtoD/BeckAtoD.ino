c4a1eca15e0da346b867c49672ee375368f7b81a
lbeck37 <lbeck37@gmail.com>
2016-09-19 19:08:16 -0600

    HP7: Got Adafruit ADS1115 library working by protecting the .h from multiple inclusions.

--- Sketches/BeckAtoD/BeckAtoD.ino
+++ Sketches/BeckAtoD/BeckAtoD.ino
@@ -1,27 +1,36 @@
-#include <Wire.h>
+//Beck 9/19/16
+//#include <Wire.h>
 #include <Adafruit_ADS1015.h>
 
 Adafruit_ADS1115 ads(0x48);
 float Voltage = 0.0;
 
+
 void setup(void)
 {
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  Serial.begin(115200);
   ads.begin();
-}
+} //setup
+
 
 void loop(void)
 {
   int16_t adc0;  // we read from the ADC, we have a sixteen bit integer as a result
 
-  adc0 = ads.readADC_SingleEnded(0);
-  Voltage = (adc0 * 0.1875)/1000;
+  for (int sChannel= 0; sChannel < 4; sChannel++) {
+     adc0 = ads.readADC_SingleEnded(sChannel);
+     Voltage = (adc0 * 0.1875)/1000;
 
-  Serial.print("AIN0: ");
-  Serial.print(adc0);
-  Serial.print("\tVoltage: ");
-  Serial.println(Voltage, 7);
-  Serial.println();
+     Serial.print("sChannel= ");
+     Serial.print(sChannel);
+     Serial.print(",\tAin= ");
+     Serial.print(adc0);
+     Serial.print(",\tVoltage: ");
+     Serial.println(Voltage, 7);
+     Serial.println();
+  } //for(int sChannel=0;
 
   delay(1000);
-}
+} //loop
+//Last line.
