9701d71d263c76b321a359593edace892e89dc1d
lbeck37 <lbeck37@gmail.com>
2016-09-25 14:40:55 -0600

    HP7: Get SF Thing Dev program going for ESP8266 and Blynk and flash blue LED on Thing.

--- Sketches/Beck_ESP8266_Blynk_First/Beck_ESP8266_Blynk_First.ino
+++ Sketches/Beck_ESP8266_Blynk_First/Beck_ESP8266_Blynk_First.ino
@@ -1,19 +1,19 @@
-// 09/21/16 Beck, Sparkfun Thing example
+// 09/25/16 Beck, Sparkfun Thing Dev example
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
-#include<Arduino.h>
+//#include<Arduino.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
 char szAuth[] = "55bce1afbf894b3bb67b7ea34f29d45a";
-#define LED     D0        // Led in NodeMCU at pin GPIO16 (D0).
-
+//#define LED     D0        // Led in NodeMCU at pin GPIO16 (D0).
+#define LED     5         // Blue LED on SparkFun Thing Dev board is at GP5
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("\nBegin ESP8266_Standalone.ino 9/21/16, Vista64");
-  pinMode(LED, OUTPUT);   // LED ESP G16 pin as output. 
+  Serial.println("\nBegin Beck_ESP8266_Blynk_First.ino 9/25/16, HP7");
+  pinMode(LED, OUTPUT);   // LED ESP G16 pin as output.
   Serial.print("LED Pin D0=c");
   Serial.println(LED);
   FlashLED(2);
@@ -38,7 +38,7 @@ void FlashLED(int sNumber){
   for(int i= 0; i < sNumber; i++){
     digitalWrite(LED, LOW);          // turn the LED on.
     delay(500);
-    digitalWrite(LED, HIGH);          // turn the LED off.  
+    digitalWrite(LED, HIGH);          // turn the LED off.
     delay(100);
   }
   delay(2000);
