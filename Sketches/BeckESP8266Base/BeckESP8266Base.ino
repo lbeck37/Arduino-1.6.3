4c7d8312cac4f64141fc1989e8673643af501e30
lbeck37 <lbeck37@gmail.com>
2016-11-29 16:56:31 -0800

    Lenny: Bring up BeckESP8266Base.ino

--- Sketches/BeckESP8266Base/BeckESP8266Base.ino
+++ Sketches/BeckESP8266Base/BeckESP8266Base.ino
@@ -1,5 +1,6 @@
 String acSketchName  = "BeckESP8266Base.ino";
-String acFileDate    = "Sept 19, 2016_HP7_A";		//Was May 15, 2016_HP7N, June 19, 2016_LBT_D
+String acFileDate    = "Nov 29, 2016_Lenny_A";
+//String acFileDate    = "Sept 19, 2016_HP7_A";		//Was May 15, 2016_HP7N, June 19, 2016_LBT_D
 
 //#define TEST_WIFI
 
@@ -12,8 +13,8 @@ static const char           acRouterName[]      = "Aspot24";
 static const char           acRouterName[]      = "C1200spot";
 static const char           acRouterPW[]        = "Qazqaz11";
 */
-static const char           acRouterName[]      = "C1200spot";
-static const char           acRouterPW[]        = "Qazqaz11";
+static const char		acRouterName[]      = "LenSpot";
+static const char		acRouterPW[]        = "Qazqaz11";
 
 
 static const char           	acMyURL[]           = "esp1101Dev";   //Beck, Dev type sensor, #1
