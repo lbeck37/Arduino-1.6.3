62477a9d75b8589f994289d2341e504c16cdacc9
lbeck37 <lbeck37@gmail.com>
2017-10-03 16:49:44 -0600

    Lenny: Working on bringing up micro SD card on WROVER-KIT

--- Sketches/BeckEspressifESP32_graphicstest/BeckEspressifESP32_graphicstest.ino
+++ Sketches/BeckEspressifESP32_graphicstest/BeckEspressifESP32_graphicstest.ino
@@ -13,14 +13,20 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
-
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "WROVER_KIT_LCD.h"
+#include "esp_wp1.h"
+#include "esp_wp2.h"
 
-#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
+#include "FS.h"
+#include "SD_MMC.h"
 
-WROVER_KIT_LCD tft;
+#define min(X, Y) 		(((X) < (Y)) ? (X) : (Y))
+#define PAUSE_DELAY		delay(2000)
+
+WROVER_KIT_LCD 		tft;
+static uint8_t 		cardType= SD_MMC.cardType();
 
 void setup() {
   Serial.begin(115200);
@@ -54,6 +60,7 @@ void setup() {
   Serial.println(F("Benchmark                Time (microseconds)"));
   delay(10);
 
+  StartSDcard();
   //TestLoop();
 
   Serial.println(F("Done!"));
@@ -69,15 +76,71 @@ void loop(void) {
     delay(1000);
   }
 */
+	TestSDcard();
   TestLoop();
   return;
 }	//loop
 
 
+void StartSDcard() {
+	Serial.println("StartSDcard(): Begin");
+	if(!SD_MMC.begin()){
+		Serial.println("Card Mount Failed");
+		return;
+	}
+	uint8_t cardType= SD_MMC.cardType();
+
+	if(cardType == CARD_NONE){
+		Serial.println("No SD_MMC card attached");
+	}
+	return;
+}	//StartSDcard
+
+
+void TestSDcard() {
+	Serial.println("TestSDcard(): Begin");
+  Serial.print("SD_MMC Card Type: ");
+  if(cardType == CARD_MMC){
+      Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+      Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+      Serial.println("SDHC");
+  } else if(cardType == CARD_NONE){
+      Serial.println("No SD_MMC card attached");
+  } else {
+      Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+  Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+
+  listDir(SD_MMC, "/", 0);
+  createDir(SD_MMC, "/mydir");
+  listDir(SD_MMC, "/", 0);
+  removeDir(SD_MMC, "/mydir");
+  listDir(SD_MMC, "/", 2);
+  writeFile(SD_MMC, "/hello.txt", "Hello ");
+  appendFile(SD_MMC, "/hello.txt", "World!\n");
+  readFile(SD_MMC, "/hello.txt");
+  deleteFile(SD_MMC, "/foo.txt");
+  renameFile(SD_MMC, "/hello.txt", "/foo.txt");
+  readFile(SD_MMC, "/foo.txt");
+  testFileIO(SD_MMC, "/test.txt");
+  Serial.printf("Total space: %lluMB\n", SD_MMC.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD_MMC.usedBytes() / (1024 * 1024));
+
+	Serial.println("TestSDcard(): End");
+	return;
+}	//TestSDcard
+
+
 void TestLoop(void) {
+  tft.setRotation(2);
+
   Serial.print(F("Screen fill              "));
   Serial.println(testFillScreen());
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Text                     "));
   Serial.println(testText());
@@ -85,42 +148,51 @@ void TestLoop(void) {
 
   Serial.print(F("Lines                    "));
   Serial.println(testLines(WROVER_CYAN));
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Horiz/Vert Lines         "));
   Serial.println(testFastLines(WROVER_RED, WROVER_BLUE));
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Rectangles (outline)     "));
   Serial.println(testRects(WROVER_GREEN));
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Rectangles (filled)      "));
   Serial.println(testFilledRects(WROVER_YELLOW, WROVER_MAGENTA));
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Circles (filled)         "));
   Serial.println(testFilledCircles(10, WROVER_MAGENTA));
 
   Serial.print(F("Circles (outline)        "));
   Serial.println(testCircles(10, WROVER_WHITE));
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Triangles (outline)      "));
   Serial.println(testTriangles());
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Triangles (filled)       "));
   Serial.println(testFilledTriangles());
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Rounded rects (outline)  "));
   Serial.println(testRoundRects());
-  delay(500);
+  PAUSE_DELAY;
 
   Serial.print(F("Rounded rects (filled)   "));
   Serial.println(testFilledRoundRects());
-  delay(500);
+  PAUSE_DELAY;
+
+  tft.setRotation(2);
+
+  tft.drawJpg(esp_wp1_jpg, esp_wp1_jpg_len);
+  PAUSE_DELAY;
+
+  tft.drawJpg(esp_wp2_jpg, esp_wp2_jpg_len);
+  PAUSE_DELAY;
+
 	return;
 }	//TestLoop
 
@@ -392,4 +464,168 @@ unsigned long testFilledRoundRects() {
 
   return micros() - start;
 }	//testFilledRoundRects
+
+
+//SD Card tests
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+}
+
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+}
+
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+}
+
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}	//testFileIO
 //Last line
