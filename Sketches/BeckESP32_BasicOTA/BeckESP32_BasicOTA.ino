65f77fb0a26497ab2b615618490cb4130d549c18
lbeck37 <lbeck37@gmail.com>
2017-10-06 15:07:37 -0600

    Lenny: More work on ArduinoOTA, eA asked for password first time and then failed each time after

--- Sketches/BeckESP32_BasicOTA/BeckESP32_BasicOTA.ino
+++ Sketches/BeckESP32_BasicOTA/BeckESP32_BasicOTA.ino
@@ -1,5 +1,6 @@
 static const char szSketchName[]  = "BeckESP32_BasicOTA.ino";
-static const char szFileDate[]    = "Oct 6, 2017, Lenny-a";
+static const char szFileDate[]    = "Oct 6, 2017, Lenny-c";
+// "See https://diyprojects.io/arduinoota-esp32-wi-fi-ota-wireless-update-arduino-ide"
 #include <BeckLogLib.h>
 #include <WiFi.h>
 #include <ESPmDNS.h>
@@ -15,7 +16,7 @@ const char* password = "Qazqaz11";
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting");
+  //Serial.println("Booting");
   Serial << endl << "setup(): Begin " << szSketchName << ", " << szFileDate << endl;
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -30,9 +31,11 @@ void setup() {
 
   // Hostname defaults to esp3232-[MAC]
   // ArduinoOTA.setHostname("myesp32");
+  ArduinoOTA.setHostname("ESP32-DevKitC");
 
   // No authentication by default
   // ArduinoOTA.setPassword("admin");
+  ArduinoOTA.setPassword("tuna37");
 
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
