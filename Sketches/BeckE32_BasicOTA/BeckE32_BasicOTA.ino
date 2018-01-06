4f9189c6cb3c34a26f90582a0b1ff060cd5d71ad
lbeck37 <lbeck37@gmail.com>
2018-01-05 17:06:06 -0700

    Lenny: Working on ESP32 OTA

--- Sketches/BeckE32_BasicOTA/BeckE32_BasicOTA.ino
+++ Sketches/BeckE32_BasicOTA/BeckE32_BasicOTA.ino
@@ -1,5 +1,5 @@
 const char szSketchName[]  = "BeckE32_BasicOTA.ino";
-const char szFileDate[]    = "Jan 4, 2018, Lenny-f, NOT Sloeber 4.2";
+const char szFileDate[]    = "Jan 5, 2018, Lenny-c, Sloeber 4.2";
 // "See https://diyprojects.io/arduinoota-esp32-wi-fi-ota-wireless-update-arduino-ide"
 #include <BeckLogLib.h>
 #include <WiFi.h>
@@ -28,7 +28,7 @@ void setup() {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
-  }
+  } //while
 
   // Port defaults to 3232
   // ArduinoOTA.setPort(3232);
@@ -60,21 +60,25 @@ void setup() {
     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
     Serial.println("Start updating " + type);
   });
+
   ArduinoOTA.onEnd([]() {
     Serial << "ArduinoOTA.onEnd(): Begin " << endl;
     Serial.println("\nEnd");
   });
+
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    Serial.printf("ArduinoOTA.onProgress(): %u%%\r", (progress / (total / 100)));
   });
+
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
+    Serial.printf("ArduinoOTA.onError(): Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
     else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
+
   Serial << "setup(): Call ArduinoOTA.begin()" << endl;
   ArduinoOTA.begin();
   Serial.println("Ready");
