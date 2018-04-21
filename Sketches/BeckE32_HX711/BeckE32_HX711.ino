77f1b7fca850f7fb96c8e83a2351fa0ed1458f14
lbeck37 <lbeck37@gmail.com>
2018-04-21 10:28:26 -0600

    Lenny: Debugging HX711

--- Sketches/BeckE32_HX711/BeckE32_HX711.ino
+++ Sketches/BeckE32_HX711/BeckE32_HX711.ino
@@ -1,5 +1,6 @@
 #include "HX711.h"
 #include <soc/rtc.h>
+#include <Streaming.h>
 
 // HX711.DOUT	- pin #A1
 // HX711.PD_SCK	- pin #A0
@@ -15,51 +16,29 @@ HX711			oPedalForce;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("\nBeckE32_HX711.ino, Apr 20, 2018-p");
+  Serial.println();
+  Serial.println("BeckE32_HX711.ino, Apr 21, 2018-f");
 
-/*
   Serial.println("setup(): Call rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M)");
   rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
-*/
 
-  Serial.println("setup(): Call oPedalForce.begin()");
   oPedalForce.begin(cHX711_DOUT, cHX711_SCK);		//Use default gain
-  Serial.println("setup(): Back from oPedalForce.begin()");
+  TakeReadings();
 
-  Serial.println("setup(): Call oPedalForce.read()");
-  Serial.print("read: \t\t");
-  Serial.println(oPedalForce.read());			// print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(oPedalForce.read_average(20));  	// print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(oPedalForce.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(oPedalForce.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
-						// by the SCALE parameter (not set yet)  
-
+  //Set the scale.
+  Serial.print("oPedalForce.set_scale(2280.f): \t\t");
   oPedalForce.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+
+  Serial.println("Call oPedalForce.tar()");
   oPedalForce.tare();				        // reset the scale to 0
 
   Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(oPedalForce.read());                 // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(oPedalForce.read_average(20));       // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(oPedalForce.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(oPedalForce.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-						// by the SCALE parameter set with set_scale
+  TakeReadings();
 
   Serial.println("Readings:");
-}
+  return;
+}	//setup
+
 
 void loop() {
   Serial.print("one reading:\t");
@@ -70,4 +49,34 @@ void loop() {
   oPedalForce.power_down();			        // put the ADC in sleep mode
   delay(5000);
   oPedalForce.power_up();
-}
+  return;
+}	//loop
+
+
+void TakeReadings(){
+	unsigned long		ulValue;
+  Serial.println("TakeReadings(): Call oPedalForce.read()");
+  //Serial.print("read: \t\t");
+  //Serial.println(oPedalForce.read());			// print a raw reading from the ADC
+  ulValue= oPedalForce.read();
+  Serial << "TakeReadings(): oPedalForce.read() returned " << ulValue << endl;
+
+  //Serial.print("oPedalForce.read_average(20): \t\t");
+  //Serial.println(oPedalForce.read_average(20));  	// print the average of 20 readings from the ADC
+  ulValue= oPedalForce.read_average(20);
+  Serial << "TakeReadings(): oPedalForce.read_average(20) returned " << ulValue << endl;
+
+  //Serial.print("oPedalForce.get_value(5): \t\t");
+  //Serial.println(oPedalForce.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
+  ulValue= oPedalForce.get_value(5);
+  Serial << "TakeReadings(): oPedalForce.get_value(5) returned " << ulValue << endl;
+
+  //Serial.print("oPedalForce.get_units(5): \t\t");
+  //Serial.println(oPedalForce.get_units(5), 1);
+  // Print the average of 5 readings from the ADC minus tare weight (not set) divided
+	// by the SCALE parameter (not set yet)
+  ulValue= oPedalForce.get_units(5);
+  Serial << "TakeReadings(): oPedalForce.get_units(5) returned " << ulValue << endl;
+	return;
+}	//TakeReadings
+//Last line
--- libraries/HX711/HX711.cpp
+++ libraries/HX711/HX711.cpp
@@ -23,12 +23,11 @@ HX711::~HX711() {
 void HX711::begin(byte dout, byte pd_sck, byte gain) {
 	PD_SCK = pd_sck;
 	DOUT = dout;
-  //Serial.println("HX711::begin(): dout= %d", dout);
-	Serial << "HX711::begin(): DOUT= " << DOUT << ", PD_SCK= " << PD_SCK << endl;
 
-  Serial.println("HX711::begin(): Call pinMode(PD_SCK, OUTPUT)");
+	Serial << "HX711::begin(): Call pinMode(" << PD_SCK << ", OUTPUT)" << endl;
 	pinMode(PD_SCK, OUTPUT);
-  Serial.println("HX711::begin(): Call pinMode(DOUT, INPUT)");
+
+	Serial << "HX711::begin(): Call pinMode(" << DOUT << ", INPUT)" << endl;
 	pinMode(DOUT, INPUT);
 
   Serial.println("HX711::begin(): Call set_gain(gain)");
@@ -40,7 +39,8 @@ void HX711::begin(byte dout, byte pd_sck, byte gain) {
 
 bool HX711::is_ready() {
 	return digitalRead(DOUT) == LOW;
-}
+}	//is_ready
+
 
 void HX711::set_gain(byte gain) {
 	switch (gain) {
@@ -53,7 +53,11 @@ void HX711::set_gain(byte gain) {
 		case 32:		// channel B, gain factor 32
 			GAIN = 2;
 			break;
-	}
+		default:
+			Serial << "HX711::set_gain(): Bad case in switch" << endl;
+			GAIN = 1;
+			break;
+	}	//switch
 
 	Serial << "HX711::set_gain(): Call digitalWrite(PD_SCK, LOW)" << endl;
 	digitalWrite(PD_SCK, LOW);
@@ -64,42 +68,44 @@ void HX711::set_gain(byte gain) {
 
 
 long HX711::read() {
+	unsigned long ulValue = 0;
+	uint8_t aucData[3] = { 0 };
+	uint8_t ucFiller = 0x00;
+
 	// wait for the chip to become ready
 	while (!is_ready()) {
 		// Will do nothing on Arduino but prevent resets of ESP8266 (Watchdog Issue)
 		yield();
-	}
-
-	unsigned long value = 0;
-	uint8_t data[3] = { 0 };
-	uint8_t filler = 0x00;
+	}	//while
 
 	// pulse the clock pin 24 times to read the data
-	data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-	data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-	data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	aucData[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	aucData[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	aucData[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
 
 	// set the channel and the gain factor for the next reading using the clock pin
 	for (unsigned int i = 0; i < GAIN; i++) {
 		digitalWrite(PD_SCK, HIGH);
 		digitalWrite(PD_SCK, LOW);
-	}
+	}	//for
 
 	// Replicate the most significant bit to pad out a 32-bit signed integer
-	if (data[2] & 0x80) {
-		filler = 0xFF;
+	if (aucData[2] & 0x80) {
+		ucFiller = 0xFF;
 	} else {
-		filler = 0x00;
+		ucFiller = 0x00;
 	}
 
 	// Construct a 32-bit signed integer
-	value = ( static_cast<unsigned long>(filler) << 24
-			| static_cast<unsigned long>(data[2]) << 16
-			| static_cast<unsigned long>(data[1]) << 8
-			| static_cast<unsigned long>(data[0]) );
+	ulValue = ( static_cast<unsigned long>(ucFiller) << 24
+			| static_cast<unsigned long>(aucData[2]) << 16
+			| static_cast<unsigned long>(aucData[1]) << 8
+			| static_cast<unsigned long>(aucData[0]) );
 
-	return static_cast<long>(value);
-}
+	Serial << "HX711::read(): Returning ulValue= " << ulValue << endl;
+	return static_cast<long>(ulValue);
+}	//read
+
 
 long HX711::read_average(byte times) {
 	long sum = 0;
@@ -108,42 +114,57 @@ long HX711::read_average(byte times) {
 		yield();
 	}
 	return sum / times;
-}
+}	//read_average
+
 
 double HX711::get_value(byte times) {
 	return read_average(times) - OFFSET;
-}
+}	//get_value
+
 
 float HX711::get_units(byte times) {
 	return get_value(times) / SCALE;
-}
+}	//get_units
+
 
 void HX711::tare(byte times) {
 	double sum = read_average(times);
 	set_offset(sum);
-}
+	return;
+}	//tare
+
 
 void HX711::set_scale(float scale) {
 	SCALE = scale;
-}
+	return;
+}	//set_scale
+
 
 float HX711::get_scale() {
 	return SCALE;
-}
+}	//get_scale
+
 
 void HX711::set_offset(long offset) {
 	OFFSET = offset;
-}
+	return;
+}	//set_offset
+
 
 long HX711::get_offset() {
 	return OFFSET;
-}
+}	//get_offset
+
 
 void HX711::power_down() {
 	digitalWrite(PD_SCK, LOW);
 	digitalWrite(PD_SCK, HIGH);
-}
+	return;
+}	//power_down
+
 
 void HX711::power_up() {
 	digitalWrite(PD_SCK, LOW);
-}
+	return;
+}	//power_up
+//Lastline.
