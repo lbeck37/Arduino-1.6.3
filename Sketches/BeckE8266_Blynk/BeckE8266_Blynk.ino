49eec858b3483bfbb37e212ac2294b1746b15330
lbeck37 <lbeck37@gmail.com>
2018-12-02 10:35:20 -0700

    Lenny: Moved some stuff around in BeckE8266_Blynk.ino

--- Sketches/BeckE8266_Blynk/BeckE8266_Blynk.ino
+++ Sketches/BeckE8266_Blynk/BeckE8266_Blynk.ino
@@ -1,5 +1,5 @@
 const char szSketchName[]  = "BeckE8266_Blynk.ino";
-const char szFileDate[]    = "Lenny 12/01/18f";
+const char szFileDate[]    = "Lenny 12/02/18a";
 
 //Uncomment out desired implementation.
 //#define FRONT_LIGHTS
@@ -323,43 +323,6 @@ void UpdateDisplay(void){
 }	//UpdateDisplay
 
 
-void ScanForI2CDevices(void){
-	byte ucError, ucAddress;
-  int nDevices;
-  nDevices = 0;
-  for(ucAddress = 1; ucAddress < 127; ucAddress++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(ucAddress);
-    ucError = Wire.endTransmission();
-
-    if (ucError == 0){
-      Serial << LOG0 << "ScanForI2CDevices(): I2C device found at address 0x";
-      if (ucAddress<16){
-        Serial.print("0");
-      }	//if(ucAddress<16)
-      Serial.println(ucAddress,HEX);
-      //Serial.println("  !");
-      nDevices++;
-    }	//if(ucError==0)
-    else if (ucError==4) {
-      //Serial.print("Unknown error at address 0x");
-      Serial << LOG0 << "ScanForI2CDevices(): Unknown error at address 0x";
-      if (ucAddress<16) {
-        Serial.print("0");
-      }	//if(ucAddress<16)
-      Serial.println(ucAddress,HEX);
-    }	//else if(ucError==4)
-  }
- if (nDevices == 0){
-    Serial.println("No I2C devices found\n");
- }	//if(nDevices==0)
-  return;
-}	//ScanForDevices
-
-
 void SetupWiFi(){
   WiFi.mode(WIFI_AP_STA);
   Serial << LOG0 << "SetupWiFi(): Call WiFi.begin("<< szRouterName << ", " << szRouterPW << ")" << endl;
@@ -428,103 +391,6 @@ String szWiFiStatus(wl_status_t eWiFiStatus){
   } //switch
   return szStatus;
 } //szWiFiStatus
-
-
-#if OTA_SERVER
-void SetupServer(void) {
-    MDNS.begin(acHostname);
-    oESP8266WebServer.on("/", HTTP_GET, [](){
-      oESP8266WebServer.sendHeader("Connection", "close");
-      oESP8266WebServer.sendHeader("Access-Control-Allow-Origin", "*");
-      oESP8266WebServer.send(200, "text/html", acServerIndex);
-    });
-    oESP8266WebServer.on("/update", HTTP_POST, []() {
-      oESP8266WebServer.sendHeader("Connection", "close");
-      oESP8266WebServer.sendHeader("Access-Control-Allow-Origin", "*");
-      oESP8266WebServer.send(200, "text/plain", (Update.hasError()) ? "Update Failed!" : "Update Successful!");
-      ESP.restart();
-    },[](){
-      HandleUpdate();
-    });
-    oESP8266WebServer.begin();
-    MDNS.addService("http", "tcp", 80);
-    Serial << LOG0 << " SetupServer(): Open http://" << acHostname << ".local to perform an OTA update" << endl;
-//#endif
-  return;
-} //SetupServer
-
-
-void HandleUpdate() {
-  //upload() returns oHttpServer._currentUpload which is an HTTPUpload struct
-  HTTPUpload& stHTTPUpload = oESP8266WebServer.upload();
-  if (stHTTPUpload.status == UPLOAD_FILE_START) {
-    PauseBlynk();
-    Serial.setDebugOutput(true);
-    WiFiUDP::stopAll();
-    uint32_t ulMaxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
-    Serial << LOG0 << " HandleUpdate(): Update status     = UPLOAD_FILE_START" << endl;
-    Serial << LOG0 << " HandleUpdate(): ulMaxSketchSpace  = " << ulMaxSketchSpace << endl;
-    Serial << LOG0 << " HandleUpdate(): Update filename   = " << stHTTPUpload.filename << endl;
-    Serial << LOG0 << " HandleUpdate(): Update name       = " << stHTTPUpload.name << endl;
-    Serial << LOG0 << " HandleUpdate(): Update type       = " << stHTTPUpload.type << endl;
-    Serial << LOG0 << " HandleUpdate(): Update totalSize  = " << stHTTPUpload.totalSize << endl;
-    Serial << LOG0 << " HandleUpdate(): Update currentSize= " << stHTTPUpload.currentSize << endl;
-    if (!Update.begin(ulMaxSketchSpace)) { //start with max available size
-      Update.printError(Serial);
-    } //if(!Update.begin(maxSketchSpace))
-  } //if(WiFi.waitForConnectResult()==WL_CONNECTED)
-  else if (stHTTPUpload.status == UPLOAD_FILE_WRITE) {
-    //Serial << LOG0 << " Handle /update HTTP_POST: UPLOAD_FILE_WRITE, upload.currentSize= " << stHTTPUpload.currentSize << endl;
-    if (Update.write(stHTTPUpload.buf, stHTTPUpload.currentSize)
-        != stHTTPUpload.currentSize) {
-      Update.printError(Serial);
-    } //if(Update.write(upload.buf, upload.currentSize) != upload.currentSize)
-  } //else if(upload.status==UPLOAD_FILE_WRITE)
-  else if (stHTTPUpload.status == UPLOAD_FILE_END) {
-    Serial << LOG0 << " HandleUpdate(): Update status     = UPLOAD_FILE_END" << endl;
-    Serial << LOG0 << " HandleUpdate(): Update filename   = " << stHTTPUpload.filename << endl;
-    Serial << LOG0 << " HandleUpdate(): Update name       = " << stHTTPUpload.name << endl;
-    Serial << LOG0 << " HandleUpdate(): Update type       = " << stHTTPUpload.type << endl;
-    Serial << LOG0 << " HandleUpdate(): Update totalSize  = " << stHTTPUpload.totalSize << endl;
-    Serial << LOG0 << " HandleUpdate(): Update currentSize= " << stHTTPUpload.currentSize << endl;
-    HandleFileEnd(stHTTPUpload);
-#if false
-    if (Update.end(true)) { //true to set the size to the current progress
-      //Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      Serial << LOG0
-          << " HandleUpdate(): UPLOAD_FILE_END (rebooting?), upload.totalSize= "
-          << stHTTPUpload.totalSize << endl;
-    } //if(Update.end(true))
-    else {
-      Update.printError(Serial);
-    } //if(Update.end(true))else
-    Serial.setDebugOutput(false);
-#endif
-  } //else if(upload.status==UPLOAD_FILE_END)
-  yield();
-  return;
-} //HandleUpdate
-
-
-void HandleFileEnd(HTTPUpload& stHTTPUploadLocal) {
-  if (Update.end(true)) { //true to set the size to the current progress
-    Serial << LOG0 << " HandleFileEnd(): UPLOAD_FILE_END (rebooting?), upload.totalSize= " << stHTTPUploadLocal.totalSize << endl;
-  } //if(Update.end(true))
-  else {
-    Update.printError(Serial);
-  } //if(Update.end(true))else
-  Serial.setDebugOutput(false);
-  return;
-} //HandleFileEnd
-
-
-void PauseBlynk() {
-    bUpdating= true;
-    Serial << LOG0 << " PauseBlynk(): Set bUpdating to " << bUpdating << endl;
-    ulUpdateTimeoutMsec= millis() + 20000;
-  return;
-} //PauseBlynk
-#endif
 
 
 int sSetupTime(){
@@ -768,6 +634,124 @@ void HandleHeatSwitch(){
 } //HandleHeatSwitch
 
 
+float fGetDegF(bool bTakeReading){
+  float fDegFReturn= 37.99;   //Value used for default in testing w/o reading sensor. fLastDegF
+  if (bTakeReading){
+    oSensors.requestTemperatures(); // Send the command to get temperatures
+    fDegFReturn= oSensors.getTempFByIndex(0);
+    fLastDegF= fDegFReturn;
+  } //if(bTakeReading)
+  else{
+    fDegFReturn= fLastDegF;
+  } //if(bTakeReading)else
+  return fDegFReturn;
+}  //fGetDegF
+
+
+float fRound(float fNum){
+  oSensors.requestTemperatures(); // Send the command to get temperatures
+  float fRounded= floor(fNum + 0.5);
+  return fRounded;
+}  //fRound
+
+
+#if OTA_SERVER
+void SetupServer(void) {
+    MDNS.begin(acHostname);
+    oESP8266WebServer.on("/", HTTP_GET, [](){
+      oESP8266WebServer.sendHeader("Connection", "close");
+      oESP8266WebServer.sendHeader("Access-Control-Allow-Origin", "*");
+      oESP8266WebServer.send(200, "text/html", acServerIndex);
+    });
+    oESP8266WebServer.on("/update", HTTP_POST, []() {
+      oESP8266WebServer.sendHeader("Connection", "close");
+      oESP8266WebServer.sendHeader("Access-Control-Allow-Origin", "*");
+      oESP8266WebServer.send(200, "text/plain", (Update.hasError()) ? "Update Failed!" : "Update Successful!");
+      ESP.restart();
+    },[](){
+      HandleUpdate();
+    });
+    oESP8266WebServer.begin();
+    MDNS.addService("http", "tcp", 80);
+    Serial << LOG0 << " SetupServer(): Open http://" << acHostname << ".local to perform an OTA update" << endl;
+//#endif
+  return;
+} //SetupServer
+
+
+void HandleUpdate() {
+  //upload() returns oHttpServer._currentUpload which is an HTTPUpload struct
+  HTTPUpload& stHTTPUpload = oESP8266WebServer.upload();
+  if (stHTTPUpload.status == UPLOAD_FILE_START) {
+    PauseBlynk();
+    Serial.setDebugOutput(true);
+    WiFiUDP::stopAll();
+    uint32_t ulMaxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+    Serial << LOG0 << " HandleUpdate(): Update status     = UPLOAD_FILE_START" << endl;
+    Serial << LOG0 << " HandleUpdate(): ulMaxSketchSpace  = " << ulMaxSketchSpace << endl;
+    Serial << LOG0 << " HandleUpdate(): Update filename   = " << stHTTPUpload.filename << endl;
+    Serial << LOG0 << " HandleUpdate(): Update name       = " << stHTTPUpload.name << endl;
+    Serial << LOG0 << " HandleUpdate(): Update type       = " << stHTTPUpload.type << endl;
+    Serial << LOG0 << " HandleUpdate(): Update totalSize  = " << stHTTPUpload.totalSize << endl;
+    Serial << LOG0 << " HandleUpdate(): Update currentSize= " << stHTTPUpload.currentSize << endl;
+    if (!Update.begin(ulMaxSketchSpace)) { //start with max available size
+      Update.printError(Serial);
+    } //if(!Update.begin(maxSketchSpace))
+  } //if(WiFi.waitForConnectResult()==WL_CONNECTED)
+  else if (stHTTPUpload.status == UPLOAD_FILE_WRITE) {
+    //Serial << LOG0 << " Handle /update HTTP_POST: UPLOAD_FILE_WRITE, upload.currentSize= " << stHTTPUpload.currentSize << endl;
+    if (Update.write(stHTTPUpload.buf, stHTTPUpload.currentSize)
+        != stHTTPUpload.currentSize) {
+      Update.printError(Serial);
+    } //if(Update.write(upload.buf, upload.currentSize) != upload.currentSize)
+  } //else if(upload.status==UPLOAD_FILE_WRITE)
+  else if (stHTTPUpload.status == UPLOAD_FILE_END) {
+    Serial << LOG0 << " HandleUpdate(): Update status     = UPLOAD_FILE_END" << endl;
+    Serial << LOG0 << " HandleUpdate(): Update filename   = " << stHTTPUpload.filename << endl;
+    Serial << LOG0 << " HandleUpdate(): Update name       = " << stHTTPUpload.name << endl;
+    Serial << LOG0 << " HandleUpdate(): Update type       = " << stHTTPUpload.type << endl;
+    Serial << LOG0 << " HandleUpdate(): Update totalSize  = " << stHTTPUpload.totalSize << endl;
+    Serial << LOG0 << " HandleUpdate(): Update currentSize= " << stHTTPUpload.currentSize << endl;
+    HandleFileEnd(stHTTPUpload);
+#if false
+    if (Update.end(true)) { //true to set the size to the current progress
+      //Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      Serial << LOG0
+          << " HandleUpdate(): UPLOAD_FILE_END (rebooting?), upload.totalSize= "
+          << stHTTPUpload.totalSize << endl;
+    } //if(Update.end(true))
+    else {
+      Update.printError(Serial);
+    } //if(Update.end(true))else
+    Serial.setDebugOutput(false);
+#endif
+  } //else if(upload.status==UPLOAD_FILE_END)
+  yield();
+  return;
+} //HandleUpdate
+
+
+void HandleFileEnd(HTTPUpload& stHTTPUploadLocal) {
+  if (Update.end(true)) { //true to set the size to the current progress
+    Serial << LOG0 << " HandleFileEnd(): UPLOAD_FILE_END (rebooting?), upload.totalSize= " << stHTTPUploadLocal.totalSize << endl;
+  } //if(Update.end(true))
+  else {
+    Update.printError(Serial);
+  } //if(Update.end(true))else
+  Serial.setDebugOutput(false);
+  return;
+} //HandleFileEnd
+
+
+void PauseBlynk() {
+    bUpdating= true;
+    Serial << LOG0 << " PauseBlynk(): Set bUpdating to " << bUpdating << endl;
+    ulUpdateTimeoutMsec= millis() + 20000;
+  return;
+} //PauseBlynk
+#endif
+
+
 void TurnHeatOn(bool bTurnOn){
   if (bTurnOn){
     String szLogString= "TurnHeatOn(): ON";
@@ -854,6 +838,43 @@ String szGetTime(long lMsec){
   szString+= String(szAddZeros(sMsec, 3)) + " ";     //Send with trailing blank to seperate from next field.
   return szString;
 } //szGetTime
+
+
+void ScanForI2CDevices(void){
+	byte ucError, ucAddress;
+  int nDevices;
+  nDevices = 0;
+  for(ucAddress = 1; ucAddress < 127; ucAddress++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(ucAddress);
+    ucError = Wire.endTransmission();
+
+    if (ucError == 0){
+      Serial << LOG0 << "ScanForI2CDevices(): I2C device found at address 0x";
+      if (ucAddress<16){
+        Serial.print("0");
+      }	//if(ucAddress<16)
+      Serial.println(ucAddress,HEX);
+      //Serial.println("  !");
+      nDevices++;
+    }	//if(ucError==0)
+    else if (ucError==4) {
+      //Serial.print("Unknown error at address 0x");
+      Serial << LOG0 << "ScanForI2CDevices(): Unknown error at address 0x";
+      if (ucAddress<16) {
+        Serial.print("0");
+      }	//if(ucAddress<16)
+      Serial.println(ucAddress,HEX);
+    }	//else if(ucError==4)
+  }
+ if (nDevices == 0){
+    Serial.println("No I2C devices found\n");
+ }	//if(nDevices==0)
+  return;
+}	//ScanForDevices
 
 
 //szAddLeadingZeros() adds 1 or 2 zeros (depending on sNumDigits being 3 or not).
@@ -966,27 +987,6 @@ void BlynkLogLine(String szString, float fValue){
   WriteTerminalLine(szTermString);
   return;
 } //BlynkLogLine:float
-
-
-float fGetDegF(bool bTakeReading){
-  float fDegFReturn= 37.99;   //Value used for default in testing w/o reading sensor. fLastDegF
-  if (bTakeReading){
-    oSensors.requestTemperatures(); // Send the command to get temperatures
-    fDegFReturn= oSensors.getTempFByIndex(0);
-    fLastDegF= fDegFReturn;
-  } //if(bTakeReading)
-  else{
-    fDegFReturn= fLastDegF;
-  } //if(bTakeReading)else
-  return fDegFReturn;
-}  //fGetDegF
-
-
-float fRound(float fNum){
-  oSensors.requestTemperatures(); // Send the command to get temperatures
-  float fRounded= floor(fNum + 0.5);
-  return fRounded;
-}  //fRound
 
 
 void SendIntToBlynk(int sVirtualPin, int sValue){
