69dd19c54c868008d28b832bd1095abe666c92a1
lbeck37 <lbeck37@gmail.com>
2019-01-04 18:58:42 -0700

    Lenny: Remove nneeded BlynkOLD dir

--- Sketches/BeckE8266_Blynk/BeckE8266_Blynk.ino
+++ Sketches/BeckE8266_Blynk/BeckE8266_Blynk.ino
@@ -1,6 +1,6 @@
 const char szSketchName[]  = "BeckE8266_Blynk.ino";
 //const char szFileDate[]    = "Lenny 12/10/18y";
-const char szFileDate[]    = "Lenny 1/4/19aa";
+const char szFileDate[]    = "Lenny 1/4/19ab";
 
 //Uncomment out desired implementation.
 //#define FRONT_LIGHTS
