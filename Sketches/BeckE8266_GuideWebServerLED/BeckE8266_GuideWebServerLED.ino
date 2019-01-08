332e22b87c1d921558d06eb12d8f293d63c914e0
lbeck37 <lbeck37@gmail.com>
2019-01-07 17:46:33 -0700

    Lenny: BeckE8266_GuideWebServerLED.ino has Access Point integrated and woring but mDNS still not working

--- Sketches/BeckE8266_GuideWebServerLED/BeckE8266_GuideWebServerLED.ino
+++ Sketches/BeckE8266_GuideWebServerLED/BeckE8266_GuideWebServerLED.ino
@@ -1,53 +1,82 @@
+const char szSketchName[]  = "BeckE8266_GuideWebServerLED.ino";
+const char szFileDate[]    = "Lenny 1/7/19j";
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WiFiMulti.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
+#include <Streaming.h>
 
-ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+ESP8266WebServer      server(80);    // Create web server that listens for HTTP request on port 80
 
-ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+static const int      led                   = 2;
+static const char     szRouterName[]        = "Aspot24";
+static const char     szRouterPW[]          = "Qazqaz11";
+static const char     szDNSName[]           = "beckdev1";
 
-const int led = 2;
+static const char     szAccessPointSSID[]   = "BeckESP8266AccessPoint";
+static const char     szAccessPointPW[]     = "Qazqaz11";
 
-void handleRoot();              // function prototypes for HTTP handlers
-void handleLED();
-void handleNotFound();
+//Not sure why only these functions need prototypes
+void SetupmDNS();
+void SetupWebServer();
 
 void setup(void){
   Serial.begin(115200);         // Start the Serial communication to send messages to the computer
   delay(10);
-  Serial.println('\n');
+  //Serial.println('\n');
+  Serial << endl << "setup(): Sketch: " << szSketchName << ", " << szFileDate << endl;
+  Serial << "setup(): Connecting to " << szRouterName << " using " << szRouterPW << endl;
 
   pinMode(led, OUTPUT);
 
-  wifiMulti.addAP("Aspot24", "Qazqaz11");   // add Wi-Fi networks you want to connect to
+  WiFi.begin(szRouterName, szRouterPW);             // Connect to the network
+  Serial << "setup(): Connecting .";
 
-  Serial.println("Connecting ...");
-  int i = 0;
-  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(250);
     Serial.print('.');
   }
-  Serial.println('\n');
-  Serial.print("Connected to ");
-  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+  Serial << "setup(): Connected to " << WiFi.SSID() << endl;
+  Serial << "setup(): IP address: " << WiFi.localIP() << endl;
 
+  SetupmDNS();
+  SetupWebServer();
+  SetupAccessPoint();
+  return;
+} //setup
+
+
+void SetupmDNS(){
+  Serial << "SetupmDNS(): Begin" << endl;
   if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
-    Serial.println("mDNS responder started");
+    Serial << "SetupmDNS(): mDNS responder started" << endl;
   } else {
-    Serial.println("Error setting up MDNS responder!");
+    Serial << "SetupmDNS(): Error setting up MDNS responder!" << endl;
   }
+  return;
+} //SetupmDNS
 
+
+void SetupWebServer(){
+  Serial << "SetupWebServer(): Begin" << endl;
   server.on("/", HTTP_GET, handleRoot);     // Call the 'handleRoot' function when a client requests URI "/"
   server.on("/LED", HTTP_POST, handleLED);  // Call the 'handleLED' function when a POST request is made to URI "/LED"
   server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
 
   server.begin();                           // Actually start the server
-  Serial.println("HTTP server started");
-}
+  Serial << "SetupWebServer(): HTTP server started" << endl;
+  return;
+} //SetupWebServer
+
+
+void SetupAccessPoint(){
+  Serial << "SetupAccessPoint(): Begin" << endl;
+  WiFi.softAP(szAccessPointSSID, szAccessPointPW);             // Start the access point
+  Serial << "SetupAccessPoint(): Access Point " << szAccessPointSSID << " started" << endl;
+  Serial << "SetupAccessPoint(): IP address: " << WiFi.softAPIP() << endl;
+} //SetupAccessPoint
+
 
 void loop(void){
   server.handleClient();                    // Listen for HTTP requests from clients
@@ -73,8 +102,10 @@ void handleLED() {                          // If a POST request is made to URI 
   digitalWrite(led,!digitalRead(led));      // Change the state of the LED
   server.sendHeader("Location","/");        // Add a header to respond with a new location for the browser to go to the home page again
   server.send(303);                         // Send it back to the browser with an HTTP status 303 (See Other) to redirect
-}
+} //handleLED
+
 
 void handleNotFound(){
   server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
-}
+} //handleNotFound
+//Last line.
