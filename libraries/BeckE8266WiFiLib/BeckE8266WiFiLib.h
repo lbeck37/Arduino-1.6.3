d38fba99bf42c486d8d9096944e1d8d55bd6020b
lbeck37 <lbeck37@gmail.com>
2019-01-16 14:29:18 -0700

    Lenny: Before adding SoftAP to BeckE8266_NtpTimeExample.ino

--- libraries/BeckE8266WiFiLib/BeckE8266WiFiLib.h
+++
@@ -1,9 +0,0 @@
-//BeckE8266WiFiLib.h, 1/13/19
-#pragma once
-#include <IPAddress.h>
-
-extern IPAddress             _oStationIPAddress;
-
-bool      bSetupWiFi          (const char szRouterName[], const char szRouterPW[]);
-void      SetupmDNS           (IPAddress oIPAddress, char* szName);
-//Last line.
--- libraries/BeckWiFiLib/BeckWiFiLib.cpp
+++ libraries/BeckWiFiLib/BeckWiFiLib.cpp
@@ -1,17 +1,15 @@
-//BeckWiFiLib.cpp, Beck Dec 15, 2018a
+//BeckWiFiLib.cpp, Beck Dec 16, 2018
 #include <BeckMiniLib.h>
 #include <BeckWiFiLib.h>
 
 void SetupWiFi(const char szRouterName[], const char szRouterPW[]){
-	//IPAddress oIPAddress;
-
   Serial << LOG0 << "SetupWiFi()(BeckWiFiLib.cpp): Call WiFi.mode(WIFI_STA)" << endl;
   WiFi.mode(WIFI_STA);
 
   Serial << LOG0 << "SetupWiFi(): Call WiFi.begin("<< szRouterName << ", " << szRouterPW << ")" << endl;
   WiFi.begin(szRouterName, szRouterPW);
-  //oIPAddress= WiFi.localIP();
-  //Serial << LOG0 << "SetupWiFi(): oIPAddress: " << oIPAddress << endl;
+  //Reports 0.0.0
+  //Serial << LOG0 << "SetupWiFi(): Local IP: " << WiFi.localIP() << endl;
   return;
 } //SetupWiFi
 //Last line.
