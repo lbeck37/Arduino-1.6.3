f8200056b9fa3eb63a965a72ed5b86836116462a
lbeck37 <lbeck37@gmail.com>
2019-01-22 15:38:40 -0700

    Lenny: Having trouble getting it to load on Dev board wich was in infinate loop and I tought was toast. Able to use Arduino IDE to load my new Beck_HelloWorld.ino setch.

---
+++ Sketches/Beck_HelloWorld/Beck_HelloWorld.ino
@@ -0,0 +1,20 @@
+//const char szSketchName[]  = "Beck_HelloWorld.ino";
+//const char szFileDate[]    = "Lenny 1/22/19a";
+
+#include "Arduino.h"
+//#include <BeckMiniLib.h>
+
+void setup(){
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("\nsetup(): Hello World!");
+  return;
+} //setup
+
+
+void loop()
+{
+  //Do nothing.
+  return;
+} //loop
+//Last line.
