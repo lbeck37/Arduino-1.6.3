ed32026a379cbe905fdb62bad8e919ac80f6f400
lbeck37 <lbeck37@gmail.com>
2019-03-01 10:32:45 -0700

    "MPU9150AHRS" display is woring and not crashing. Display values need to cange back to floats

--- Sketches/Beck_MPU9150AHRS/Beck_MPU9150AHRS.ino
+++ Sketches/Beck_MPU9150AHRS/Beck_MPU9150AHRS.ino
@@ -1,5 +1,5 @@
 const char szSketchName[]  = "MPU9150AHRS";
-const char szFileDate[]    = " 03/01/19c";
+const char szFileDate[]    = " 03/01/19g";
 /* MPU9150 Basic Example Code
  by: Kris Winer
  date: March 1, 2014
@@ -21,148 +21,10 @@ const char szFileDate[]    = " 03/01/19c";
  */
 #include <BeckMiniLib.h>
 #include <BeckLogLib.h>
-//#include <IMUQuaternionFilters.h>
-#include <Beck_IMUQuaternionFilters.h> //Beck
+#include <Beck_IMUdefines.h>
+#include <Beck_IMUQuaternionFilters.h>
 #include "Wire.h"
 #include <Adafruit_SSD1306.h>     ////For I2C OLED display
-
-// Define registers per MPU6050, Register Map and Descriptions, Rev 4.2, 08/19/2013 6 DOF Motion sensor fusion device
-// Invensense Inc., www.invensense.com
-// See also MPU-9150 Register Map and Descriptions, Revision 4.0, RM-MPU-9150A-00, 9/12/2012 for registers not listed in
-// above document; the MPU6050 and MPU 9150 are virtually identical but the latter has an on-board magnetic sensor
-//
-//Magnetometer Registers
-#define WHO_AM_I_AK8975A 0x00 // should return 0x48
-#define INFO             0x01
-#define AK8975A_ST1      0x02  // data ready status bit 0
-#define AK8975A_ADDRESS  0x0C
-#define AK8975A_XOUT_L   0x03  // data
-#define AK8975A_XOUT_H   0x04
-#define AK8975A_YOUT_L   0x05
-#define AK8975A_YOUT_H   0x06
-#define AK8975A_ZOUT_L   0x07
-#define AK8975A_ZOUT_H   0x08
-#define AK8975A_ST2      0x09  // Data overflow bit 3 and data read error status bit 2
-#define AK8975A_CNTL     0x0A  // Power down (0000), single-measurement (0001), self-test (1000) and Fuse ROM (1111) modes on bits 3:0
-#define AK8975A_ASTC     0x0C  // Self test control
-#define AK8975A_ASAX     0x10  // Fuse ROM x-axis sensitivity adjustment value
-#define AK8975A_ASAY     0x11  // Fuse ROM y-axis sensitivity adjustment value
-#define AK8975A_ASAZ     0x12  // Fuse ROM z-axis sensitivity adjustment value
-
-#define XGOFFS_TC        0x00 // Bit 7 PWR_MODE, bits 6:1 XG_OFFS_TC, bit 0 OTP_BNK_VLD
-#define YGOFFS_TC        0x01
-#define ZGOFFS_TC        0x02
-#define X_FINE_GAIN      0x03 // [7:0] fine gain
-#define Y_FINE_GAIN      0x04
-#define Z_FINE_GAIN      0x05
-#define XA_OFFSET_H      0x06 // User-defined trim values for accelerometer
-#define XA_OFFSET_L_TC   0x07
-#define YA_OFFSET_H      0x08
-#define YA_OFFSET_L_TC   0x09
-#define ZA_OFFSET_H      0x0A
-#define ZA_OFFSET_L_TC   0x0B
-#define SELF_TEST_X      0x0D
-#define SELF_TEST_Y      0x0E
-#define SELF_TEST_Z      0x0F
-#define SELF_TEST_A      0x10
-#define XG_OFFS_USRH     0x13  // User-defined trim values for gyroscope, populate with calibration routine
-#define XG_OFFS_USRL     0x14
-#define YG_OFFS_USRH     0x15
-#define YG_OFFS_USRL     0x16
-#define ZG_OFFS_USRH     0x17
-#define ZG_OFFS_USRL     0x18
-#define SMPLRT_DIV       0x19
-#define CONFIG           0x1A
-#define GYRO_CONFIG      0x1B
-#define ACCEL_CONFIG     0x1C
-#define FF_THR           0x1D  // Free-fall
-#define FF_DUR           0x1E  // Free-fall
-#define MOT_THR          0x1F  // Motion detection threshold bits [7:0]
-#define MOT_DUR          0x20  // Duration counter threshold for motion interrupt generation, 1 kHz rate, LSB = 1 ms
-#define ZMOT_THR         0x21  // Zero-motion detection threshold bits [7:0]
-#define ZRMOT_DUR        0x22  // Duration counter threshold for zero motion interrupt generation, 16 Hz rate, LSB = 64 ms
-#define FIFO_EN          0x23
-#define I2C_MST_CTRL     0x24
-#define I2C_SLV0_ADDR    0x25
-#define I2C_SLV0_REG     0x26
-#define I2C_SLV0_CTRL    0x27
-#define I2C_SLV1_ADDR    0x28
-#define I2C_SLV1_REG     0x29
-#define I2C_SLV1_CTRL    0x2A
-#define I2C_SLV2_ADDR    0x2B
-#define I2C_SLV2_REG     0x2C
-#define I2C_SLV2_CTRL    0x2D
-#define I2C_SLV3_ADDR    0x2E
-#define I2C_SLV3_REG     0x2F
-#define I2C_SLV3_CTRL    0x30
-#define I2C_SLV4_ADDR    0x31
-#define I2C_SLV4_REG     0x32
-#define I2C_SLV4_DO      0x33
-#define I2C_SLV4_CTRL    0x34
-#define I2C_SLV4_DI      0x35
-#define I2C_MST_STATUS   0x36
-#define INT_PIN_CFG      0x37
-#define INT_ENABLE       0x38
-#define DMP_INT_STATUS   0x39  // Check DMP interrupt
-#define INT_STATUS       0x3A
-#define ACCEL_XOUT_H     0x3B
-#define ACCEL_XOUT_L     0x3C
-#define ACCEL_YOUT_H     0x3D
-#define ACCEL_YOUT_L     0x3E
-#define ACCEL_ZOUT_H     0x3F
-#define ACCEL_ZOUT_L     0x40
-#define TEMP_OUT_H       0x41
-#define TEMP_OUT_L       0x42
-#define GYRO_XOUT_H      0x43
-#define GYRO_XOUT_L      0x44
-#define GYRO_YOUT_H      0x45
-#define GYRO_YOUT_L      0x46
-#define GYRO_ZOUT_H      0x47
-#define GYRO_ZOUT_L      0x48
-#define EXT_SENS_DATA_00 0x49
-#define EXT_SENS_DATA_01 0x4A
-#define EXT_SENS_DATA_02 0x4B
-#define EXT_SENS_DATA_03 0x4C
-#define EXT_SENS_DATA_04 0x4D
-#define EXT_SENS_DATA_05 0x4E
-#define EXT_SENS_DATA_06 0x4F
-#define EXT_SENS_DATA_07 0x50
-#define EXT_SENS_DATA_08 0x51
-#define EXT_SENS_DATA_09 0x52
-#define EXT_SENS_DATA_10 0x53
-#define EXT_SENS_DATA_11 0x54
-#define EXT_SENS_DATA_12 0x55
-#define EXT_SENS_DATA_13 0x56
-#define EXT_SENS_DATA_14 0x57
-#define EXT_SENS_DATA_15 0x58
-#define EXT_SENS_DATA_16 0x59
-#define EXT_SENS_DATA_17 0x5A
-#define EXT_SENS_DATA_18 0x5B
-#define EXT_SENS_DATA_19 0x5C
-#define EXT_SENS_DATA_20 0x5D
-#define EXT_SENS_DATA_21 0x5E
-#define EXT_SENS_DATA_22 0x5F
-#define EXT_SENS_DATA_23 0x60
-#define MOT_DETECT_STATUS 0x61
-#define I2C_SLV0_DO      0x63
-#define I2C_SLV1_DO      0x64
-#define I2C_SLV2_DO      0x65
-#define I2C_SLV3_DO      0x66
-#define I2C_MST_DELAY_CTRL 0x67
-#define SIGNAL_PATH_RESET  0x68
-#define MOT_DETECT_CTRL   0x69
-#define USER_CTRL        0x6A  // Bit 7 enable DMP, bit 3 reset DMP
-#define PWR_MGMT_1       0x6B // Device defaults to the SLEEP mode
-#define PWR_MGMT_2       0x6C
-#define DMP_BANK         0x6D  // Activates a specific bank in the DMP
-#define DMP_RW_PNT       0x6E  // Set read/write pointer to a specific start address in specified DMP bank
-#define DMP_REG          0x6F  // Register in DMP from which to read or to which to write
-#define DMP_REG_1        0x70
-#define DMP_REG_2        0x71
-#define FIFO_COUNTH      0x72
-#define FIFO_COUNTL      0x73
-#define FIFO_R_W         0x74
-#define WHO_AM_I_MPU9150 0x75 // Should return 0x68
 
 // Using the GY-521 breakout board, I set ADO to 0 by grounding through a 4k7 resistor
 // Seven-bit device address is 110100 for ADO = 0 and 110101 for ADO = 1
@@ -225,7 +87,6 @@ float     roll;
 uint32_t  lastUpdate  = 0; // used to calculate integration interval
 uint32_t  firstUpdate = 0; // used to calculate integration interval
 uint32_t  Now         = 0;        // used to calculate integration interval
-//float ax, ay, az, gx, gy, gz, mx, my, mz; // variables to hold latest sensor data values
 float     ax= 0.00;
 float     ay= 0.00;
 float     az= 0.00;
@@ -313,14 +174,10 @@ void setup(){
   Serial << LOG0 << "setup(): WHO_AM_I= " << c << ", should be 0x68 (104d)" << endl;
   if (c == 0x68){            // WHO_AM_I should always be 0x68
     Serial << LOG0 << "setup(): MPU9150 is online" << endl;
-    //Serial << LOG0 << "setup(): Call MPU6050SelfTest()" << endl;
-    MPU6050SelfTest(SelfTest); // Start by performing self test
 
-    //Serial << LOG0 << "setup(): Call calibrateMPU9150()" << endl;
-    calibrateMPU9150(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
-
-    //Serial << LOG0 << "setup(): Call initMPU9150()" << endl;
-    initMPU9150(); // Inititalize and configure accelerometer and gyroscope
+    MPU6050SelfTest   (SelfTest); // Start by performing self test
+    calibrateMPU9150  (gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
+    initMPU9150       (); // Inititalize and configure accelerometer and gyroscope
     Serial << LOG0 << "setup(): MPU9150 initialized for active data mode" << endl;
 
     // Read the WHO_AM_I register of the magnetometer, this is a good test of communication
@@ -439,15 +296,6 @@ void loop()
     delt_t= millis() - count;
     if (delt_t > ulPrintPeriodMsec) { // update LCD once per half-second independent of read rate
       digitalWrite(blinkPin, blinkOn);
-
-/*
-      Serial << LOG0 << "loop():    ax= " << (int)1000*ax << ", ay= " << (int)1000*ay <<
-          ", az= " << (int)1000*az << endl;
-      Serial << LOG0 << "loop():    gx= " << gx << ", gy= " << gy << ", gz= " << gz << endl;
-      Serial << LOG0 << "loop():    mx= " << (int)mx << ", my= " << (int)my << ", mz= " << (int)mz << endl;
-      Serial << LOG0 << "loop():    q0= " << q[0] << ", qx= " << q[1] << ", qy= " << q[2] << ", qz= " << q[3] << endl;
-*/
-
       // Define output variables from updated quaternion---these are Tait-Bryan angles, commonly used in aircraft orientation.
       // In this coordinate system, the positive z-axis is down toward Earth.
       // Yaw is the angle between Sensor x-axis and Earth magnetic North (or true North if corrected for local declination, looking down on the sensor positive yaw is counterclockwise.
@@ -551,13 +399,8 @@ void DisplayData(void){
    wLine= 0;
    wYStart= wLine * wDotsPerLine;
    display.setCursor(0, wYStart);
-/*
-   display.print("P "); display.print(pitch, 1);
-   display.print(", R "); display.print(roll, 1);
-*/
    display.print("P ");   display.print(afAccGyroMagPRY[ePRY][ePitch], 1);
    display.print(", R "); display.print(afAccGyroMagPRY[ePRY][eRoll], 1);
-   //display.display();
 
    wLine= 1;
    wYStart= wLine * wDotsPerLine;
@@ -565,46 +408,18 @@ void DisplayData(void){
    display.print("Y "); display.print(afAccGyroMagPRY[ePRY][eYaw], 1);
    display.print(", "); display.print(fTemperature, 1);
    display.print(" C");
-   //display.display();
-
-/*
-   wLine= 1;
-   wYStart= wLine * wDotsPerLine;
-   display.setCursor(0, wYStart); display.print(" x     y     z  ");
-*/
 
    wLine= 2;
    wYStart= wLine * wDotsPerLine;
-/*
-   display.setCursor(0,  wYStart); display.print((int16_t)(1000*ax));
-   display.setCursor(30, wYStart); display.print((int16_t)(1000*ay));
-   display.setCursor(60, wYStart); display.print((int16_t)(1000*az));
-   display.setCursor(100, wYStart); display.print("mg");
-   display.display();
-*/
    display.setCursor(0,  wYStart); display.print(asAccGyroMagMilliInt[eAccel][eX]);
    display.setCursor(30, wYStart); display.print(asAccGyroMagMilliInt[eAccel][eY]);
    display.setCursor(60, wYStart); display.print(asAccGyroMagMilliInt[eAccel][eZ]);
 
    wLine= 3;
    wYStart= wLine * wDotsPerLine;
-/*
-   display.setCursor(0,  wYStart); display.print((int16_t)(gx));
-   display.setCursor(30, wYStart); display.print((int16_t)(gy));
-   display.setCursor(60, wYStart); display.print((int16_t)(gz));
-   display.setCursor(90, wYStart); display.print("Deg/s");
-   display.display();
-*/
    display.setCursor(0,  wYStart); display.print(asAccGyroMagMilliInt[eGyro][eX]);
    display.setCursor(30, wYStart); display.print(asAccGyroMagMilliInt[eGyro][eY]);
    display.setCursor(60, wYStart); display.print(asAccGyroMagMilliInt[eGyro][eZ]);
-/*
-   wLine= 4;
-   wYStart= wLine * wDotsPerLine;
-   display.setCursor(0, wYStart); display.print("Temperature= ");
-   display.print(temperature, 1); display.print(" C");
-   display.display();
-*/
    wLine= 5;
    wYStart= wLine * wDotsPerLine;
    display.setCursor(0, wYStart);
