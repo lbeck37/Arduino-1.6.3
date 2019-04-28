1b58ecb470162af044975a92f03e909e096f313f
lbeck37 <lbeck37@gmail.com>
2019-04-27 20:04:36 -0600

    Added http.get() but not working

--- Sketches/libraries/BeckAngularTestPages/BeckAngularTestPagesHTML.h
+++ Sketches/libraries/BeckAngularTestPages/BeckAngularTestPagesHTML.h
@@ -4,86 +4,36 @@
 const char* acAngularTestPagesHTML= R"(
 <!DOCTYPE HTML><html>
 <!doctype html>
-<!-- File: chapter5/item-service-using-service/index.html -->
+<!-- File: chapter4/simple-form.html -->
 <html ng-app="notesApp">
-<body ng-controller="MainCtrl as mainCtrl">
-  <h1>Hello Controllers!</h1>
-  <button ng-click="mainCtrl.open('first') ">Open First</button>
-  <button ng-click="mainCtrl.open('second') ">Open Second</button>
-  <div ng-switch on="mainCtrl.tab">
+<head><title>Notes App</title></head>
+<body ng-controller="MainCtrl as ctrl">
 
-    <div ng-switch-when="first">
-      <div ng-controller="SubCtrl as ctrl">
-        <h3>First tab</h3>
-        <ul>
-          <li ng-repeat="item in ctrl.list() ">
-            <span ng-bind="item.label"></span>
-          </li>
-        </ul>
+  <form ng-submit="ctrl.submit() ">
+    <input type="text" ng-model="ctrl.Current.DegF">
+    You typed {{ctrl.Current.DegF}}
 
-        <button ng-click="ctrl.add() ">Add More Items</button>
-      </div>
+    <input type="submit" value="DoIt">
+  </form>
 
-    </div>
-    <div ng-switch-when="second">
-      <div ng-controller="SubCtrl as ctrl">
-        <h3>Second tab</h3>
-        <ul>
-          <li ng-repeat="item in ctrl.list() ">
-            <span ng-bind="item.label"></span>
-          </li>
-        </ul>
-
-        <button ng-click="ctrl.add() ">Add More Items</button>
-      </div>
-    </div>
-  </div>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.js"></script>
-  <script src="app.js"></script>
-
-<script>
-// File: chapter5/item-service-using-service/app.js
-
-function ItemService() {
-  var items = [
-    {id: 1, label: 'Item 0'},
-    {id: 2, label: 'Item 1'}
-  ];
-  this.list = function() {
-    return items;
-  };
-  this.add = function(item) {
-    items.push(item);
-  };
-}
-
-angular.module('notesApp', [])
-  .controller('MainCtrl', [function() {
-    var self = this;
-    self.tab = 'first';
-    self.open = function(tab) {
-      self.tab = tab;
-    };
-  }])
-  .controller('SubCtrl',
-      ['ItemService', function(ItemService) {
-    var self = this;
-    self.list = function() {
-      return ItemService.list();
-    };
-
-    self.add = function() {
-      ItemService.add({
-        id: self.list().length + 1,
-        label: 'Item ' + self.list().length
+<script
+  src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.js">
+</script>
+<script type="text/javascript">
+  angular.module('notesApp', [])
+    .controller('MainCtrl', [function() {
+      var Biota = this;
+      $http.get('/LastDegF').then(function(response) {
+        Biota.DegF = response.data;
+      }, function(errResponse) {
+        console.error('Error while fetching notes');
       });
-    };
-  }])
-  .service('ItemService', [ItemService]);
+      Biota.submit = function() {
+        console.log('User clicked DoIt with ', Biota.Current);
+      };
+    }]);
 </script>
 </body>
-
 </html>
 )";
 
