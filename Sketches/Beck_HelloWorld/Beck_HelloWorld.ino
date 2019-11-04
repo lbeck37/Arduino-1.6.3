ed503f7f8f74fceba21d96f15fcdaa4b69e8ae0c
lbeck37 <lbeck37@users.noreply.github.com>
2019-11-03 19:02:15 -0700

    Lenny: Moved Bek_HelloWorld bsck to Sketches

---
+++ Sketches/Beck_HelloWorld/Beck_HelloWorld.ino
@@ -0,0 +1,20 @@
+#include "Arduino.h"
+
+void setup(){
+  Serial.begin(115200);
+  delay(500);
+<<<<<<< HEAD
+  Serial.println("\nBeck_HelloWord.ino: setup(): Hello World! 11/2/19c");
+=======
+  Serial.println("\nBeck_HelloWorld_CPP.cpp: setup(): Hello World! 11/1/19b");
+>>>>>>> 302ffb0bf1fb2cd4ae12ada7fd24612ff7744001
+  return;
+} //setup
+
+
+void loop()
+{
+  //Do nothing.
+  return;
+} //loop
+//Last line.
