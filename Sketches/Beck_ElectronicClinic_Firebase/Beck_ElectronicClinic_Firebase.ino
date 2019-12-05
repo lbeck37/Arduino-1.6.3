41e271e68638972b84d2d2d3828bd5519a43332a
lbeck37 <lbeck37@users.noreply.github.com>
2019-12-05 12:47:52 -0700

    Lenny

--- Sketches/Beck_ElectronicClinic_Firebase/Beck_ElectronicClinic_Firebase.ino
+++ Sketches/Beck_ElectronicClinic_Firebase/Beck_ElectronicClinic_Firebase.ino
@@ -1,4 +1,4 @@
-//Beck_ElectronicClinic_Firebase.ino, 12/5/19e
+//Beck_ElectronicClinic_Firebase.ino, 12/5/19f
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 #include <FirebaseArduino.h>
@@ -27,35 +27,40 @@ String myString;
 int vr = A0; // variable resistor connected
 int sdata = 0; // The variable resistor value will be stored in sdata.
 
-void setup()
-{
+void setup(){
   // Debug console
   Serial.begin(115200);
   pinMode(vr ,INPUT);
+
   // connect to wifi.
   pinMode(D0,OUTPUT);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED)
-      {
+  while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(500);
-      }
+  } //while
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-   Firebase.setString("Variable/Value","fahad");
-}
+  Firebase.setString("Variable/Value","fahad");
+  if (!Firebase.success()){
+    Serial.print(Firebase.error());
+  } //if(!Firebase.success())
+} //setup
 
-void loop()
-{
 
-sdata = analogRead(vr);
-myString = String(sdata);
-Serial.println(myString);
-Firebase.setString("Variable/Value",myString);
-delay(1000);
-}
+void loop(){
+  sdata = analogRead(vr);
+  myString = String(sdata);
+  Serial.println(myString);
+  Firebase.setString("Variable/Value",myString);
+  if (!Firebase.success()){
+    Serial.print(Firebase.error());
+  } //if(!Firebase.success())
+delay(5000);
+} //loop
+//Last line.
