9f3e13edf864b8fd442f374d1d1b08188f3d8983
lbeck37 <lbeck37@users.noreply.github.com>
2020-01-12 19:50:54 -0700

    Lenny: After fooling around with DNS

--- Sketches/Beck_AutoConnectWithFeedback/Beck_AutoConnectWithFeedback.ino
+++ Sketches/Beck_AutoConnectWithFeedback/Beck_AutoConnectWithFeedback.ino
@@ -1,5 +1,5 @@
 const char szSketchName[]  = "Beck_AutoConnectWithFeedback.ino";
-const char szFileDate[]    = "1/12/20d";
+const char szFileDate[]    = "1/12/20f";
 
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
@@ -9,11 +9,19 @@ const char szFileDate[]    = "1/12/20d";
 #include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
 #include <Streaming.h>
 
+/*
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("Entered config mode");
   Serial.println(WiFi.softAPIP());
   //if you used auto generated SSID, print it
   Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+*/
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial << "configModeCallback(): WiFi.softAPIP()= " << WiFi.softAPIP() << endl;
+  //if you used auto generated SSID, print it
+  Serial << "configModeCallback(): myWiFiManager->getConfigPortalSSID()= " <<
+  		myWiFiManager->getConfigPortalSSID() << endl;
 }
 
 void setup() {
@@ -24,17 +32,22 @@ void setup() {
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
+  WiFiManager 	wifiManager;
   //reset settings - for testing
-  //wifiManager.resetSettings();
+  if (true){
+  	Serial << "setup(): Call resetSettings()" << endl;
+  	wifiManager.resetSettings();
+  }	//
 
   //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  Serial << "setup(): Call setAPCallback()" << endl;
   wifiManager.setAPCallback(configModeCallback);
 
   //fetches ssid and pass and tries to connect
   //if it does not connect it starts an access point with the specified name
   //here  "AutoConnectAP"
   //and goes into a blocking loop awaiting configuration
+  Serial << "seup(): Call wifiManager.autoConnect() repeatably" << endl;
   if(!wifiManager.autoConnect()) {
     Serial.println("failed to connect and hit timeout");
     //reset and try again, or maybe put it to deep sleep
@@ -45,8 +58,8 @@ void setup() {
   Serial << "SetupWiFi():  SSID= " << WiFi.SSID() << ", IP address: " << WiFi.localIP() << endl;
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
-
-}
+  return;
+}	//setup
 
 void loop() {
   // put your main code here, to run repeatedly:
