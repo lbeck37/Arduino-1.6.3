30fa7ea89083e9896773e74e2488be811e016c1b
lbeck37 <lbeck37@users.noreply.github.com>
2020-01-22 09:17:51 -0700

    Lenny: After try to get HelloWorld to compile inludes.

--- Sketches/Beck_HelloWorld/Beck_HelloWorld.ino
+++ Sketches/Beck_HelloWorld/Beck_HelloWorld.ino
@@ -1,12 +1,13 @@
 #include "Arduino.h"
 //#include "FirebaseESP8266.h"
 //#include <ESP8266WiFi.h>
+#include "ArduinoJson.h"
 #include "FirebaseArduino.h"
 
 void setup(){
   Serial.begin(115200);
   delay(500);
-  Serial.println("\nBeck_HelloWorld.ino: setup(): Hello World! 1/21/20e");
+  Serial.println("\nBeck_HelloWorld.ino: setup(): Hello World! 1/22/20c");
   return;
 } //setup
 
