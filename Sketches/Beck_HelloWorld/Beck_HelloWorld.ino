07516ecffa085d141f3be7a51b73996b99fe6403
Larry Beck <lbeck37@gmail.com>
2021-09-27 07:27:46 -0600

    Beck: Got the ESP32-CAM going using AI Thinker sketch

--- Sketches/Beck_HelloWorld/Beck_HelloWorld.ino
+++ Sketches/Beck_HelloWorld/Beck_HelloWorld.ino
@@ -2,13 +2,13 @@
 #include "Arduino.h"
 //#include "FirebaseESP8266.h"
 //#include <ESP8266WiFi.h>
-#include "ArduinoJson.h"
-#include "FirebaseArduino.h"
+//#include "ArduinoJson.h"
+//#include "FirebaseArduino.h"
 
 void setup(){
   Serial.begin(115200);
   delay(500);
-  Serial.println("\nBeck_HelloWorld.ino: setup(): Hello World! 2/3/20h_ 4.3.2");
+  Serial.println("\nBeck_HelloWorld.ino: setup(): Hello World! 9/26/21b");
   return;
 } //setup
 
