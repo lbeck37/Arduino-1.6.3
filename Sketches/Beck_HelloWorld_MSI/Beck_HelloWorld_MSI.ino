1121cb0954a93f0768b075c405896cde45a4e715
Larry Beck <lbeck37@gmail.com>
2021-09-28 18:24:35 -0600

    Beck- Changed name in attempt to fix git merge problem.

--- Sketches/Beck_HelloWorld_MSI/Beck_HelloWorld_MSI.ino
+++ Sketches/Beck_HelloWorld_MSI/Beck_HelloWorld_MSI.ino
@@ -1,4 +1,4 @@
-//Beck_HelloWorld.ino
+//Beck_HelloWorld_MSI.ino
 #include "Arduino.h"
 //#include "FirebaseESP8266.h"
 //#include <ESP8266WiFi.h>
@@ -8,7 +8,7 @@
 void setup(){
   Serial.begin(115200);
   delay(500);
-  Serial.println("\nBeck_HelloWorld.ino: setup(): Hello World! 9/28/21a");
+  Serial.println("\nBeck_HelloWorld.ino: setup(): Hello World! 9/28/21b");
   return;
 } //setup
 
