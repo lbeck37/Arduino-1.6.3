fa91915178b2363bd16e8550b39bf3d7e3453e29
Larry Beck <LBeck37@gmail.com>
2021-09-30 18:27:27 -0600

    Beck: Bringing up Grammy, the LG gram 16" i7-11gen

--- Sketches/BeckE32_CameraWebServer/BeckE32_CameraWebServer.ino
+++ Sketches/BeckE32_CameraWebServer/BeckE32_CameraWebServer.ino
@@ -1,4 +1,4 @@
-// Beck, 9/26/21b BeckE32_CameraWebServer.ino
+// Beck, 9/29/21a BeckE32_CameraWebServer.ino
 #include "esp_camera.h"
 #include <WiFi.h>
 
@@ -18,7 +18,26 @@
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
 //#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
-#include "camera_pins.h"
+//#include "camera_pins.h"
+//#elif defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
 
 const char* ssid = "Aspot24";
 const char* password = "Qazqaz11";
@@ -28,7 +47,7 @@ void startCameraServer();
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Serial.println();
+  Serial.println("\nBeck, 9/29/21a BeckE32_CameraWebServer.ino");
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
