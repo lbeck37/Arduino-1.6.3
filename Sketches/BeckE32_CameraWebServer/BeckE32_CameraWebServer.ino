28fd12736c4116f702443714610b5f1426286361
Larry Beck <LBeck37@gmail.com>
2021-10-01 12:58:25 -0600

    Beck: Copied code from camera_pins.h to .ino

--- Sketches/BeckE32_CameraWebServer/BeckE32_CameraWebServer.ino
+++ Sketches/BeckE32_CameraWebServer/BeckE32_CameraWebServer.ino
@@ -1,6 +1,8 @@
-// Beck, 10/1/21a BeckE32_CameraWebServer.ino
+const char szSketchName[]  = "BeckE32_CameraWebServer.ino";
+const char szFileDate[]    = "10/1/21c";
 #include "esp_camera.h"
 #include <WiFi.h>
+#include <Streaming.h>
 
 //
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
@@ -16,6 +18,25 @@
 //#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#elif defined(CAMERA_MODEL_AI_THINKER)    //Copied from camera_pins.h
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
 //#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
 #include "camera_pins.h"
@@ -28,7 +49,8 @@ void startCameraServer();
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
-  Serial.println("\nBeck, 9/29/21a BeckE32_CameraWebServer.ino");
+  //Serial.println("\nBeck, 9/29/21a BeckE32_CameraWebServer.ino");
+  Serial << endl<< "setup(): Begin " << szSketchName << ", " << szFileDate << endl;
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
